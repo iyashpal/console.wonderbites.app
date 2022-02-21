b887d3a1d1adf74d61db57018e09f72cf9e4d380
Karan Singh Guleriya <himekaranguleria@gmail.com>
2022-02-21 12:55:13 +0530

    minor fix

--- app/Controllers/Http/Admin/HomeController.ts
+++ app/Controllers/Http/Admin/HomeController.ts
@@ -3,7 +3,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class HomeController {
 
 
-    public show({ view, request }: HttpContextContract) {
+    public show({ view }: HttpContextContract) {
 
         return view.render('welcome')
 
