f03891b804b0e025fa02725ad6e0164463d676e1
Yash Pal <iyashpal.dev@gmail.com>
2022-02-24 17:40:42 +0530

    Environment validation added.

--- env.ts
+++ env.ts
@@ -15,6 +15,9 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
+	/************************************************
+	 * Application
+	 ************************************************/
 	HOST: Env.schema.string({ format: 'host' }),
 	PORT: Env.schema.number(),
 	APP_KEY: Env.schema.string(),
@@ -23,4 +26,14 @@ export default Env.rules({
 	SESSION_DRIVER: Env.schema.string(),
 	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+
+	/************************************************
+	 * Database
+	 ************************************************/
+	DB_CONNECTION: Env.schema.string(),
+	PG_HOST: Env.schema.string({ format: 'host' }),
+	PG_PORT: Env.schema.number(),
+	PG_USER: Env.schema.string(),
+	PG_PASSWORD: Env.schema.string.optional(),
+	PG_DB_NAME: Env.schema.string(),
 })
