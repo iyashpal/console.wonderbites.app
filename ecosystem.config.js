46471d393822d4a281481b376db8bfbeed154895
Yash Pal <iyashpal.dev@gmail.com>
2022-02-25 12:06:17 +0530

    PM2 config file updated with post update scripts.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,19 @@
 module.exports = {
-  apps: [{
-    name: "wonderbites",
-    script: "./build/server.js",
-    instances: 'max',
-    exec_mode: 'cluster',
-    autorestart: true,
-  }]
+  apps: [
+    {
+      name: "admin.wonderbites",
+      script: "./build/server.js",
+      watch: true,
+      autorestart: true,
+      max_restarts: 20,
+      restart_delay: 10000,
+      max_memory_restart: '200M',
+      instances: 1,
+      exec_mode: "cluster",
+      kill_timeout: 3000,
+      cwd: "/home/ubuntu/sites/wonderbites.com/",
+      env: { NODE_ENV: "production", PORT: 8080 },
+      post_update: ['yarn install', 'yarn build', 'cd ./build && yarn install --production'],
+    }
+  ]
 }
