6806a27456d674ed46cbef26ef9a9b3d80d54b68
Yash Pal <iyashpal.dev@gmail.com>
2022-02-25 21:28:49 +0530

    Another experiment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,19 +20,13 @@ module.exports = {
 
       kill_timeout: 3000,
 
-      watch: ['build'],
+      watch: ['./', 'build'],
 
       ignore_watch: ["node_modules"],
 
       cwd: "/home/ubuntu/sites/wonderbites.com/",
-      
-      pid_file: "/home/ubuntu/sites/wonderbites.com/.logs/pid.log",
 
-      out_file: "/home/ubuntu/sites/wonderbites.com/.logs/output.log",
-
-      error_file: "/home/ubuntu/sites/wonderbites.com/.logs/error.log",
-
-      post_update: ['yarn install', 'yarn build', 'cd ./build && yarn install --production'],
+      post_update: ['yarn install', 'yarn build'],
 
       env: { NODE_ENV: "production", ENV_PATH: "/home/ubuntu/sites/wonderbites.com/.env", PORT: 8080 },
 
