c00844e87c43f4f892c9736312105bbd102b9bcf
Yash Pal <iyashpal.dev@gmail.com>
2022-03-01 11:12:09 +0530

    Code refactoring and UI fixes.

---
+++ resources/js/layouts/Components/SidebarLinks.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        url: "/",
+        label: "Dashboard",
+        icon: "space-dashboard"
+    },
+    {
+        url: "/categories",
+        label: "Categories",
+        icon: "category"
+    },
+    {
+        url: "/cuisines",
+        label: "Cuisines",
+        icon: "dinner-dining"
+    },
+    {
+        url: "/testimonials",
+        label: "Testimonials",
+        icon: "comment"
+    },
+    {
+        url: "/users",
+        label: "Users",
+        icon: "group"
+    },
+]
