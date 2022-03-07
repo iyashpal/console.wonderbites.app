42574a57503316e2da17efe7ca823b9c61e5a6a7
neerajbrandsonify <neeraj@brandsonify.com>
2022-03-07 12:54:57 +0530

    changes

--- app/Controllers/Http/API/UsersController.ts
+++ app/Controllers/Http/API/UsersController.ts
@@ -33,8 +33,8 @@ export default class UsersController {
     }
 
     await user.merge({
-      first_name: request.input('first_name'),
-      last_name: request.input('last_name'),
+      first_name: request.input('first_name')? request.input('first_name') : user.first_name ,
+      last_name: request.input('last_name')? request.input('last_name') : user.last_name ,
       image_path: image ? image!.fileName : user.image_path,
     }).save().then((user) => response.status(200).json(user))
   }
