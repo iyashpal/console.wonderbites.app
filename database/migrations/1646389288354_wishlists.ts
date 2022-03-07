a285e63507cbeef72dfa6c01cedeee96c44461eb
neerajbrandsonify <neeraj@brandsonify.com>
2022-03-07 13:32:00 +0530

    Lastest apis changes

--- database/migrations/1646389288354_wishlists.ts
+++ database/migrations/1646389288354_wishlists.ts
@@ -6,14 +6,11 @@ export default class Wishlists extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-<<<<<<< HEAD:database/migrations/1646389288354_wishlists.ts
-=======
 
       table.integer('user_id').unsigned().notNullable().references('users.id').onDelete('CASCADE')
 
       table.string('ip_address').nullable()
 
->>>>>>> 42574a57503316e2da17efe7ca823b9c61e5a6a7:database/migrations/1646046472166_wishlists.ts
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
