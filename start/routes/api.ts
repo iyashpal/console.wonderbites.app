4ea1bbf6bc13aa41550fc21484e59f09c01116db
neerajbrandsonify <neeraj@brandsonify.com>
2022-03-07 13:18:52 +0530

    route file issue

--- start/routes/api.ts
+++ start/routes/api.ts
@@ -24,10 +24,6 @@ Route.group(() => {
 
     Route.resource('productimages', 'API/ProductImagesController')
 
-<<<<<<< HEAD
-        Route.resource('addresses', 'API/Profile/AddressesController');
-        Route.resource('wishlists', 'API/WishlistsController');
-=======
     Route.get('getcart', 'API/CartsController.getcart').as('getcart')
 
     //Route.resource('cart', 'API/CartsController');
@@ -36,42 +32,14 @@ Route.group(() => {
 
     //Route.get('/products/productbycategory', "API/ProductsController.productbycategory").as('productbycategory');
   }).middleware('auth_api')
->>>>>>> 42574a57503316e2da17efe7ca823b9c61e5a6a7
 
   Route.get('about', 'API/HomeController.about').as('about')
 
-<<<<<<< HEAD
-    }).middleware('auth_api')
-    Route.resource('cart', 'API/CartsController');
-    Route.resource('cuisines', 'API/CuisinesController');
-    Route.get('/getcart/:device_token', 'API/CartsController.getcart').as('getcart')
-   // Route.get('/getcart', "API/CartsController.getcart").as('getcart')
-   Route.resource('categories', 'API/CategoriesController');
-
-    Route.resource('products', 'API/ProductsController');
-
-    Route.resource('productimages', 'API/ProductImagesController');
-
-    Route.get('about', "API/HomeController.about").as('about')
-
-    Route.get('terms', "API/HomeController.terms").as('terms')
-
-    Route.get('privacy_policy', "API/HomeController.privacy_policy").as('privacy_policy')
-
-    Route.get('content_policy', "API/HomeController.content_policy").as('content_policy')
-
-    Route.get('change_email', "API/HomeController.change_email").as('change_email')
-
-    Route.get('delete_account', "API/HomeController.delete_account").as('delete_account')
-
-    Route.get('my_subscriptions', "API/HomeController.my_subscriptions").as('my_subscriptions')
-=======
   Route.get('terms', 'API/HomeController.terms').as('terms')
 
   Route.get('privacy_policy', 'API/HomeController.privacy_policy').as('privacy_policy')
 
   Route.get('content_policy', 'API/HomeController.content_policy').as('content_policy')
->>>>>>> 42574a57503316e2da17efe7ca823b9c61e5a6a7
 
   Route.get('settings', 'API/HomeController.settings').as('settings')
 
