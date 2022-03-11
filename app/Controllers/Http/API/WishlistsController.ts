111d3840c81f1fbd774c74314e09e76bae569542
neerajbrandsonify <neeraj@brandsonify.com>
2022-03-11 18:43:58 +0530

    Change in admin dasboard for categories and wishlist api

--- app/Controllers/Http/API/WishlistsController.ts
+++ app/Controllers/Http/API/WishlistsController.ts
@@ -1,69 +1,53 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ProductWishlist from 'App/Models/ProductWishlist'
 //import { schema, rules } from "@ioc:Adonis/Core/Validator"
 import Wishlist from 'App/Models/Wishlist'
 export default class WishlistsController {
-    
-        public async index({ response }: HttpContextContract) {
-          try {
-            
-            const wishlists = await Wishlist.all()
-            response.status(200).json(wishlists)
-      
-          } catch (error) {
-            response.badRequest(error.messages)
-          }
-        }
-
-        public async store({ auth, request, response }: HttpContextContract) {
-            try {
-        
-              const user = await auth.use('api').authenticate()
-             if(user.id){
-              try {
-            
-                const wishlist_data =  { user_id : user.id, product_id : request.input('product_id') }
-        
-                const wishlist = await Wishlist.create(wishlist_data)
-        
-                // Send response
-                response.status(200).json(wishlist)
-        
-              } catch (error) {
-        
-                response.badRequest(error.message)
-        
-              }
-                }
-            } catch (error) {
-        
-              response.unauthorized({ message: "Unauthenticated" })
-        
-            }
-          }
-
-          public async getwishlist({ auth,response }: HttpContextContract) {
-            try {
-                
-              const user = await auth.use('api').authenticate()
-             if(user.id){
-              try {
-            
-                const wishlist = await Wishlist.query() 
-     .where('user_id', user.id);
-        
-                // Send response
-                response.status(200).json(wishlist)
-        
-              } catch (error) {
-        
-                response.badRequest(error.message)
-        
-              }
-                }
-            } catch (error) {
-        
-              response.unauthorized({ message: "Unauthenticated" })
-        
-            }
-          }   
+  public async index ({ response }: HttpContextContract) {
+    try {
+      const wishlists = await Wishlist.all()
+      response.status(200).json(wishlists)
+    } catch (error) {
+      response.badRequest(error.messages)
+    }
   }
+  public async store ({ auth, request, response }: HttpContextContract) {
+    try {
+      const user = await auth.use('api').authenticate()
+      if(user.id){
+        try {
+          const wishlistdata = { user_id : user.id, ip_address : request.input('ip_address') }
+          const wishlist = await Wishlist.create(wishlistdata)
+          if(request.input('product_id')){
+            const productids = request.input('product_id')
+            for(let i in productids) {
+              await ProductWishlist.create({product_id: productids[i] , wishlist_id : wishlist.id })
+            }
+          }
+
+          response.status(200).json(wishlist)
+        } catch (error) {
+          response.badRequest(error.message)
+        }
+      }
+    } catch (error){
+      response.unauthorized({ message: 'Unauthenticated' })
+    }
+  }
+
+  public async getwishlist ({ auth,response }: HttpContextContract) {
+    try {
+      const user = await auth.use('api').authenticate()
+      if(user.id){
+        try{
+          const wishlist = await Wishlist.query() .where('user_id', user.id)
+          response.status(200).json(wishlist)
+        } catch (error){
+          response.badRequest(error.message)
+        }
+      }
+    } catch (error) {
+      response.unauthorized({ message: 'Unauthenticated' })
+    }
+  }
+}
--- app/Models/CategoryProduct.ts
+++ app/Models/CategoryProduct.ts
@@ -4,7 +4,10 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 export default class CategoryProduct extends BaseModel {
   @column({ isPrimary: true })
   public id: number
-
+  @column()
+  public category_id: number
+  @column()
+  public product_id: number
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
