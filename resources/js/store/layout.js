6fa71e45095ac828bd605b7a2a1488c06fd0da26
iyashpal <iyashpal.dev@gmail.com>
2022-03-21 06:33:50 +0530

    New: Breadcrumb and title synchronization via pinia.

--- resources/js/store/layout.js
+++ resources/js/store/layout.js
@@ -3,17 +3,36 @@ import { defineStore } from 'pinia'
 const useLayoutStore = defineStore('layout', {
   state: () => ({
     sidebar: false,
+    title: '',
+    breadcrumb: [],
   }),
 
   getters: {
     isSidebarEnabled: (state) => state.sidebar,
+
+    hasBreadcrumb: (state) => !!state.breadcrumb.length,
+
+    backLinkOfBreadcrumb (state) {
+      let link = state.breadcrumb.find(({ backlink }) => backlink)
+
+      return link ? link : state.breadcrumb[0]
+    },
   },
 
   actions: {
-    toggleSidebar (payload) {
+    leSidebar (payload) {
       this.sidebar = payload ? payload : !this.sidebar
     },
+
+    syncTitle (payload) {
+      this.title = payload ? payload : ''
+    },
+
+    syncBreadcrumb (payload) {
+      this.breadcrumb = payload ? payload : {}
+    },
   },
+
 })
 
 export default useLayoutStore
