b692bceaf81bbf2a0b7a15d09201c63e0ebcb378
Yash Pal <iyashpal.dev@gmail.com>
2022-04-11 08:08:00 +0530

    Cart functionality completed.

--- app/Controllers/Http/API/CartsController.ts
+++ app/Controllers/Http/API/CartsController.ts
@@ -1,8 +1,15 @@
 import Cart from 'App/Models/Cart'
-import { isNull } from '@poppinss/utils/build/src/Helpers/types'
+import User from 'App/Models/User'
+import { RequestContract } from '@ioc:Adonis/Core/Request'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { isNull, isUndefined } from '@poppinss/utils/build/src/Helpers/types'
 
 export default class CartsController {
+  /**
+   * Show loggedin/guest user cart.
+   * 
+   * @param param0 HttpContextContract Request payload
+   */
   public async show ({ request, auth, response }: HttpContextContract) {
     await auth.use('api').check()
 
@@ -10,7 +17,7 @@ export default class CartsController {
       await auth.use('api').authenticate()
     }
 
-    const cart = await this.getCart({ request, user: auth.use('api').user! })
+    const cart = await this.cart(request, auth.use('api').user!)
 
     await cart.load('products')
     await cart.load('ingridients')
@@ -18,6 +25,11 @@ export default class CartsController {
     response.json(cart)
   }
 
+  /**
+   * Update loggedin/guest user cart.
+   * 
+   * @param param0 {HttpContextContract} Request payload.
+   */
   public async update ({ auth, request, response }: HttpContextContract) {
     await auth.use('api').check()
 
@@ -25,9 +37,17 @@ export default class CartsController {
       await auth.use('api').authenticate()
     }
 
-    const cart = await this.getCart({ request, user: auth.use('api').user! })
+    const cart = await this.cart(request, auth.use('api').user!)
 
-    // Adding products to cart will go here.
+    // Add products to cart.
+    if (request.input('action') === 'SYNC') {
+      await this.syncToCart(request, cart)
+    }
+
+    // Remove products from cart.
+    if (request.input('action') === 'DETACH') {
+      await this.detachFromCart(request, cart)
+    }
 
     await cart.load('products')
     await cart.load('ingridients')
@@ -35,8 +55,15 @@ export default class CartsController {
     response.json(cart)
   }
 
-  protected async getCart ({ request, user }): Promise<Cart> {
-    if ([null, undefined].includes(user)) {
+  /**
+   * Get cart of current loggedin/guest user.
+   * 
+   * @param request RequestContract
+   * @param user User
+   * @returns Cart
+   */
+  protected async cart (request: RequestContract, user: User): Promise<Cart> {
+    if (isNull(user) || isUndefined(user)) {
       const guestCart = await Cart.query().whereNull('user_id').where('ip_address', request.ip()).first()
 
       if (isNull(guestCart)) {
@@ -49,9 +76,45 @@ export default class CartsController {
     await user.load('cart')
 
     if (isNull(user.cart)) {
-      return await user.related('cart').create({ ip_address: request.ip() })
+      return await user.related('cart').create({ ipAddress: request.ip() })
     }
 
     return user.cart
+  }
+
+  /**
+   * Synchronize products with cart.
+   * 
+   * @param request RequestContract 
+   * @param cart Cart
+   */
+  protected async syncToCart (request: RequestContract, cart: Cart) {
+    // Add products to cart
+    if (request.input('products')) {
+      await cart.related('products').sync(request.input('products'), false)
+    }
+
+    // Add ingridients to cart
+    if (request.input('ingridients')) {
+      await cart.related('ingridients').sync(request.input('ingridients'), false)
+    }
+  }
+
+  /**
+   * Remove products from cart.
+   * 
+   * @param request RequestContract
+   * @param cart Cart
+   */
+  protected async detachFromCart (request: RequestContract, cart: Cart) {
+    // Remove products from cart.
+    if (request.input('products')) {
+      await cart.related('products').detach(request.input('products'))
+    }
+
+    // Remove products from cart.
+    if (request.input('ingridients')) {
+      await cart.related('ingridients').detach(request.input('ingridients'))
+    }
   }
 }
