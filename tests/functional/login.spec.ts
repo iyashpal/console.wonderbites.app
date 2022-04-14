fad9fa3d3701d3fe6a362d6243a55f450c26691b
Yash Pal <iyashpal.dev@gmail.com>
2022-04-14 22:09:55 +0530

    WIP: Login test cases.

---
+++ tests/functional/login.spec.ts
@@ -0,0 +1,12 @@
+import { test } from '@japa/runner'
+
+test('Guest users can access login page.', async ({ client }) => {
+  const response = await client.get('/login')
+
+  response.assertStatus(200)
+  response.assertTextIncludes('Login')
+  response.assertTextIncludes('Email Address')
+  response.assertTextIncludes('Password')
+  response.assertTextIncludes('Remember me')
+  response.assertTextIncludes('Forgot your password?')
+})
