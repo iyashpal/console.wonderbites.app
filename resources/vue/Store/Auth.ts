d8065eb61a0df9079eb3a798d9dea1c74167d809
Yash Pal <iyashpal.dev@gmail.com>
2022-07-19 15:31:58 +0530

    Code refactoring and js to ts conversion.

--- resources/vue/Store/Auth.ts
+++ resources/vue/Store/Auth.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 const useAuthStore = defineStore('auth', {
   state: () => ({
-    user: {},
+    user: <{ id?: number }>{},
   }),
 
   getters: {
--- resources/vue/routes/WebRoutes.js
+++
@@ -1,7 +0,0 @@
-export default [
-    {
-        path: "/",
-        name: "user.index",
-        // components: () => import(/* webpackChunkName: "home_page" */ "Vue/Views/Index.vue")
-    }
-]
