909355166750ec6bcf14feaf8160cfadcbda9541
Yash Pal <iyashpal.dev@gmail.com>
2022-09-21 15:54:54 +0530

    Japa plugin added.

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -6,6 +6,7 @@
  */
 
 import type { Config } from '@japa/runner'
+import { expectTypeOf } from '@japa/expect-type'
 import TestUtils from '@ioc:Adonis/Core/TestUtils'
 import { assert, runFailedTests, specReporter, apiClient } from '@japa/preset-adonis'
 
@@ -20,7 +21,7 @@ import { assert, runFailedTests, specReporter, apiClient } from '@japa/preset-ad
 | Feel free to remove existing plugins or add more.
 |
 */
-export const plugins: Config['plugins'] = [assert(), runFailedTests(), apiClient()]
+export const plugins: Config['plugins'] = [assert(), runFailedTests(), apiClient(), expectTypeOf()]
 
 /*
 |--------------------------------------------------------------------------
