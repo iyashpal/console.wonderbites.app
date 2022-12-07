fc19e3981e3543a7468bee5d6b0d6ede8acec6b3
Yash Pal <iyashpal.dev@gmail.com>
2022-12-07 17:21:22 +0530

    Code refactoring.

--- app/Helpers/ExceptionResponse.ts
+++ app/Helpers/ExceptionResponse.ts
@@ -1,20 +1,60 @@
 import { ResponseContract } from '@ioc:Adonis/Core/Response'
 
 export default class ExceptionResponse {
+  /**
+   * Exception name
+   * 
+   * @var string
+   */
   public name: string
 
+  /**
+   * Exception debug help.
+   * 
+   * @var string
+   */
   public help?: string
 
+  /**
+   * Exception code defined in framework.
+   * 
+   * @var string
+   */
   public code?: string
 
+  /**
+   * Exception status code.
+   * 
+   * @var number
+   */
   public status: number
 
+  /**
+   * Exception files stack.
+   * 
+   * @var string
+   */
   public stack?: string
 
+  /**
+   * Exception error message.
+   * 
+   * @var string
+   */
   public message: string
 
+  /**
+   * Exception messages (specifically used for validation errors.)
+   * 
+   * @var any[]
+   */
   public messages?: any[]
 
+  /**
+   * Request response instance.
+   * 
+   * @var ResponseContract
+   */
   protected response: ResponseContract
 
   constructor (response: ResponseContract, error: any) {
@@ -28,21 +68,29 @@ export default class ExceptionResponse {
     this.messages = error.messages
   }
 
-  public resolve (resolveType: string = 'json') {
+  /**
+   * Resolve the request response.
+   * 
+   * @param resolveType Exception response type.
+   * @returns void
+   */
+  public resolve (data?: any, resolveType: string = 'json') {
     switch (resolveType) {
       case 'json':
-        return this.json()
+        return this.json(data)
     }
   }
 
-  private json () {
-    this.response.status(this.status).json({
-      name: this.name,
-      code: this.code,
-      help: this.help,
-      status: this.status,
-      message: this.message,
-      messages: this.messages,
-    })
+  /**
+   * Return the json response.
+   * 
+   * @param data any
+   */
+  private json (data: any) {
+    this.response.status(this.status)
+      .json({
+        name: this.name, code: this.code, help: this.help, status: this.status,
+        message: this.message, messages: this.messages, ...data,
+      })
   }
 }
