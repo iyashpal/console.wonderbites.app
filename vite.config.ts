ee392f4662edd3ab870d7da1df937ac0ce6a16d3
Yash Pal <iyashpal.dev@gmail.com>
2023-01-06 12:03:32 +0530

    Minor fixes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import Adonis from '@adonisjs/vite-plugin-adonis'
 export default defineConfig(({ command }) => ({
-    plugins: [
-        Adonis({
-            // Write files to this directory
-            outputPath: './public/assets',
+  plugins: [
+    Adonis({
+      // Write files to this directory
+      outputPath: './public/assets',
 
-            // Prefix the following to the output URL
-            publicPath: '/assets',
+      // Prefix the following to the output URL
+      publicPath: '/assets',
 
-            // entryPoints: ['']
-        })
-    ]
-})
+      entryPoints: {
+        app: ['resources/js/app.ts'],
+      },
+    }),
+  ],
+}))
