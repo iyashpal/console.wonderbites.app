39a98021082342222fe120783d52b762309b80fa
Yash Pal <iyashpal.dev@gmail.com>
2023-01-10 11:36:56 +0530

    Vite Asset Manager Removed

--- resources/app/main.ts
+++
@@ -1,5 +0,0 @@
-import App from './App.vue'
-import '@/assets/scss/app.scss'
-import { createApp } from 'vue'
-
-createApp(App).mount('#app')
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./resources/**/*.{html,js,ts,vue,edge}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
