0c367cb56e1aaf650aac4ea8611fbc028d415e1f
Yash Pal <iyashpal.dev@gmail.com>
2023-01-19 10:32:44 +0530

    Dashboard icon updated.

--- resources/react/components/icons/DashboardIcon.tsx
+++ resources/react/components/icons/DashboardIcon.tsx
@@ -1,7 +1,7 @@
 export default function DashboardIcon(props) {
   return <>
     <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 2.04762V21.9524M1 1H23V23H1V1ZM12.44 9.38095H22.12H12.44ZM1.88 14.619H11.56H1.88Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M12 2.95238V21.0476M2 2H22V22H2V2ZM12.4 9.61905H21.2H12.4ZM2.8 14.381H11.6H2.8Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   </>
 }
