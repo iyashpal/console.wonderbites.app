cb840d183921e8cdfc853a05b71f510951582da2
Yash Pal <iyashpal.dev@gmail.com>
2023-02-28 19:22:17 +0530

    chore(new) Store category test cases added.

--- tests/functional/core/categories/store.spec.ts
+++ tests/functional/core/categories/store.spec.ts
@@ -1,5 +1,5 @@
 import { test } from '@japa/runner'
-import {UserFactory} from 'Database/factories'
+import {CategoryFactory, UserFactory} from 'Database/factories'
 import Database from '@ioc:Adonis/Lucid/Database'
 
 test.group('Core categories store', (group) => {
@@ -8,28 +8,144 @@ test.group('Core categories store', (group) => {
     return () => Database.rollbackGlobalTransaction()
   })
 
-  test('it do not allow access to a guest user.', async ({client, route}) => {
-    const response = await client.get(route('core.categories.store'))
+  test('it throws 401 error code to a guest user.', async ({client, route}) => {
+    const response = await client.post(route('core.categories.store'))
 
     response.assertStatus(401)
 
     response.assertBodyContains({message: 'Unauthorized access'})
   }).tags(['@core', '@core.categories.store'])
 
-  test('it do not allow access to none management user.', async ({client, route}) => {
+  test('it throws 401 error code to a none management user.', async ({client, route}) => {
     const user = await UserFactory.create()
-
-    const response = await client.get(route('core.categories.store')).guard('api').loginAs(user)
+    const response = await client.post(route('core.categories.store')).guard('api').loginAs(user)
 
     response.assertStatus(401)
     response.assertBodyContains({message: 'Unauthorized access'})
   }).tags(['@core', '@core.categories.store'])
 
-  test('it allows access to a management user.', async ({client, route}) => {
+  test('it throws 200 status code to a management user.', async ({client, route}) => {
     const user = await UserFactory.with('role').create()
 
-    const response = await client.get(route('core.categories.store')).guard('api').loginAs(user)
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: category.name,
+        description: category.description,
+        type: category.type,
+        parent: category.parent,
+        status: category.status,
+      })
 
     response.assertStatus(200)
+  }).tags(['@core', '@core.categories.store'])
+
+  test('it throws 422 validation error when name is missing or empty.', async ({client, route}) => {
+    const user = await UserFactory.with('role').create()
+
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: '',
+        type: category.type,
+        parent: category.parent,
+        status: category.status,
+      })
+
+    response.assertStatus(422)
+    response.assertBodyContains({errors: {name: 'required validation failed'}})
+  }).tags(['@core', '@core.categories.store'])
+
+  test('it throws 422 validation error when type is missing or empty.', async ({client, route}) => {
+    const user = await UserFactory.with('role').create()
+
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: category.name,
+        type: '',
+        parent: category.parent,
+        status: category.status,
+      })
+
+    response.assertStatus(422)
+    response.assertBodyContains({errors: {type: 'required validation failed'}})
+  }).tags(['@core', '@core.categories.store'])
+
+  test('it throws 422 validation error when parent id is invalid.', async ({client, route}) => {
+    const user = await UserFactory.with('role').create()
+
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: category.name,
+        type: category.type,
+        parent: 0,
+        status: category.status,
+      })
+
+    response.assertStatus(422)
+    response.assertBodyContains({errors: {parent: 'exists validation failure'}})
+  }).tags(['@core', '@core.categories.store'])
+
+  test('it throws 422 validation error when status is missing or empty.', async ({client, route}) => {
+    const user = await UserFactory.with('role').create()
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: category.name,
+        type: category.type,
+        parent: category.parent,
+      })
+
+    response.assertStatus(422)
+    response.assertBodyContains({errors: {status: 'required validation failed'}})
+  }).tags(['@core', '@core.categories.store'])
+
+  test('it throws 422 validation error when status is missing or empty.', async ({client, route}) => {
+    const user = await UserFactory.with('role').create()
+
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: category.name,
+        type: category.type,
+        parent: category.parent,
+      })
+
+    response.assertStatus(422)
+    response.assertBodyContains({errors: {status: 'required validation failed'}})
+  }).tags(['@core', '@core.categories.store'])
+
+  test('it passes the validation when parent is null.', async ({client, route}) => {
+    const user = await UserFactory.with('role').create()
+
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: category.name,
+        type: category.type,
+        parent: null,
+        status: category.status,
+      })
+
+    response.assertStatus(200)
+  }).tags(['@core', '@core.categories.store'])
+
+  test('it stores the category data after passing the validation rules.', async ({client, route}) => {
+    const user = await UserFactory.with('role').create()
+
+    const category = await CategoryFactory.create()
+    const response = await client.post(route('core.categories.store'))
+      .guard('api').loginAs(user).json({
+        name: 'Demo',
+        type: category.type,
+        parent: null,
+        status: category.status,
+      })
+
+    response.assertStatus(200)
+
+    response.assertBodyContains({name: 'Demo'})
   }).tags(['@core', '@core.categories.store'])
 })
