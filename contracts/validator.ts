2bfeecbfc04f7c3a19e017ba4c03a81ff24cfcb8
Yash Pal <iyashpal.dev@gmail.com>
2023-02-28 17:36:54 +0530

    chore(new) Custom validator setup files added.

---
+++ contracts/validator.ts
@@ -0,0 +1,4 @@
+declare module '@ioc:Adonis/Core/Validator' {
+  interface Rules {
+  }
+}
---
+++ start/validator.ts
@@ -0,0 +1,10 @@
+/*
+|--------------------------------------------------------------------------
+| Preloaded File
+|--------------------------------------------------------------------------
+|
+| Any code written inside this file will be executed during the application
+| boot.
+|
+*/
+// import { validator } from '@ioc:Adonis/Core/Validator'
