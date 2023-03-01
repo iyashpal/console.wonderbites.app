0b7c099a23e5936252c9179289f2a0a68fbb6fbb
Yash Pal <iyashpal.dev@gmail.com>
2023-03-01 18:29:46 +0530

    chore(new) Global modal component added.

--- resources/react/components/Modal.tsx
+++ resources/react/components/Modal.tsx
@@ -1,55 +1,30 @@
-import {Fragment} from 'react'
+import {classNames} from "@/helpers";
+import React, {Fragment} from 'react'
 import {Dialog, Transition} from '@headlessui/react'
 
+type ModalProps = {
+  children: any,
+  show: boolean,
+  className?: string,
+  onClose?: () => void,
+}
 
-export default function Modal({show = false, onClose, type = 'md'}: { show: boolean, onClose: () => void, type?: string }) {
+export default function Modal({show = false, onClose, children, className = ''}: ModalProps) {
+
   return <>
     <Transition appear show={show} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
+
+      <Dialog as="div" className="relative z-10" onClose={onClose ?? (() => {})}>
+
+        <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
           <div className="fixed inset-0 bg-black bg-opacity-25"/>
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900"
-                >
-                  Payment successful
-                </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent
-                    you an email with all of the details of your order.
-                  </p>
-                </div>
-
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={onClose}
-                  >
-                    Got it, thanks!
-                  </button>
-                </div>
+            <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
+              <Dialog.Panel {...classNames('w-full relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all', className)}>
+                {children}
               </Dialog.Panel>
             </Transition.Child>
           </div>
