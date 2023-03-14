5d4ebabc5c80dc0ba894add35449aa8f8fa7e2df
Yash Pal <iyashpal.dev@gmail.com>
2023-03-14 20:00:59 +0530

    chore(fix) table checkbox component re-defined with forwardRef.

--- resources/react/components/Index/ThCheck.tsx
+++ resources/react/components/Index/ThCheck.tsx
@@ -1,5 +1,16 @@
-export default function ThCheck(props) {
-  return <th scope="col" className={["relative w-12 px-6 sm:w-16 sm:px-8", props.className].join(' ')}>
-    <input type="checkbox" className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-primary sm:left-6"/>
-  </th>
+import {ChangeEvent, forwardRef, ForwardedRef} from "react";
+
+type CheckPropsType = {
+  value?: number,
+  className?: string,
+  isChecked?: boolean,
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void,
 }
+
+const ThCheck = forwardRef(({className, isChecked, onChange, value}: CheckPropsType, forwardedRef: ForwardedRef<HTMLInputElement>) => (
+  <th scope="col" className={["relative w-12 px-6 sm:w-16 sm:px-8", className].join(' ')}>
+    <input type="checkbox" ref={forwardedRef} checked={isChecked} onChange={onChange} value={value} className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-primary sm:left-6"/>
+  </th>
+))
+
+export default ThCheck
