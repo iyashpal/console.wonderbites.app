e517b1328e79470dea58aa1d08a0d58078169422
Yash Pal <iyashpal.dev@gmail.com>
2023-03-20 13:01:51 +0530

    chore(fix) Where tests were failing due to change of request response.

--- tests/functional/core/ingredients/update.spec.ts
+++ tests/functional/core/ingredients/update.spec.ts
@@ -9,7 +9,7 @@ test.group('Core [ingredients.update]', (group) => {
     return () => Database.rollbackGlobalTransaction()
   })
 
-  test('it do not allow access to a guest user.', async ({client, route}) => {
+  test('it throws 401 status code when the user is not logged in.', async ({client, route}) => {
     const ingredient = await IngredientFactory.create()
     const response = await client.put(route('core.ingredients.update', ingredient))
 
@@ -18,7 +18,7 @@ test.group('Core [ingredients.update]', (group) => {
     response.assertBodyContains({message: 'Unauthorized access'})
   }).tags(['@core', '@core.ingredients.update'])
 
-  test('it do not allow access to none management user.', async ({client, route}) => {
+  test('it throws 401 status code when the user is logged in but missing the role.', async ({client, route}) => {
     const user = await UserFactory.create()
     const ingredient = await IngredientFactory.create()
 
@@ -28,13 +28,16 @@ test.group('Core [ingredients.update]', (group) => {
     response.assertBodyContains({message: 'Unauthorized access'})
   }).tags(['@core', '@core.ingredients.update'])
 
-  test('it allows a management user to update ingredient.', async ({client, route}) => {
-    const ingredient = await IngredientFactory.create()
+  test('it updates product with 200 status code when user owns a role.', async ({client, route}) => {
     const user = await UserFactory.with('role').create()
+    const ingredient = await IngredientFactory.with('categories', 1).create()
+
+    const [category] = ingredient.categories
 
     const response = await client.put(route('core.ingredients.update', ingredient))
       .guard('api').loginAs(user)
       .fields({
+        categoryId: category.id,
         name: 'Demo Name',
         description: ingredient.description,
         price: ingredient.price,
