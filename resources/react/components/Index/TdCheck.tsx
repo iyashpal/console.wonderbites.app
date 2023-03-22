dfaaa885a7575848439bf51d2e387545a86378d0
Yash Pal <iyashpal.dev@gmail.com>
2023-03-22 16:59:58 +0530

    chore(fix) Table checkbox defaultChecked prop console error fix.

--- resources/react/components/Index/TdCheck.tsx
+++ resources/react/components/Index/TdCheck.tsx
@@ -11,7 +11,7 @@ const TdCheck = forwardRef(({checked, onChange, value}: CheckPropsType, forwarde
   return (
     <td className="relative w-12 px-6 sm:w-16 sm:px-8">
       {checked && (<div className="absolute inset-y-0 left-0 w-0.5 bg-red-600"/>)}
-      <input ref={forwardedRef} checked={checked} type="checkbox" onChange={onChange} value={value} className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-primary sm:left-6"/>
+      <input ref={forwardedRef} defaultChecked={checked} type="checkbox" onChange={onChange} value={value} className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-primary sm:left-6"/>
     </td>
   )
 })
