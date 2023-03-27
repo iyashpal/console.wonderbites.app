a7037e301859e4d37504aeb8a3e57bfcd7764424
Yash Pal <iyashpal.dev@gmail.com>
2023-03-27 18:27:02 +0530

    chore(refactor) Product images functionality in progress.

--- resources/react/views/products/ShowProduct.tsx
+++ resources/react/views/products/ShowProduct.tsx
@@ -51,7 +51,26 @@ export default function ShowProduct() {
               {name: 'Price', value: `${product.price}L`},
               {name: 'Category', value: category?.name ?? '-'},
               {name: 'Description', value: product.description, onModal: true},
-              {name: 'Image', value: <img alt={product.name} src={product.thumbnail_url} className="w-10 h-10 rounded-full"/>}
+              {
+                name: 'Images', value: <>
+                  <div className="flex items-center space-x-2">
+                    <div className="flex flex-shrink-0 -space-x-1">
+
+                      <img className="h-6 w-6 max-w-none rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="Dries Vincent"/>
+
+                      <img className="h-6 w-6 max-w-none rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="Lindsay Walton"/>
+
+                      <img className="h-6 w-6 max-w-none rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="Courtney Henry"/>
+
+                      <img className="h-6 w-6 max-w-none rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="Tom Cook"/>
+
+                    </div>
+
+                    <span className="flex-shrink-0 text-xs font-medium leading-5">+8</span>
+                  </div>
+                  {/*<img alt={product.name} src={product.thumbnail_url} className="w-10 h-10 rounded-full"/>*/}
+                </>
+              }
             ]}
           />
 
