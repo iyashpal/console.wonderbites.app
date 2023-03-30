624038cee23ff6874c4147e01ea8e429ab032771
Yash Pal <iyashpal.dev@gmail.com>
2023-03-30 18:28:39 +0530

    chore(new) User role type added.

---
+++ resources/react/types/models/Role.ts
@@ -0,0 +1,9 @@
+export default interface Role {
+  id: number,
+  slug: string,
+  title: string,
+  description: string,
+  scope: object,
+  created_at: string,
+  updated_at: string,
+}
--- resources/react/types/models/index.ts
+++ resources/react/types/models/index.ts
@@ -1,4 +1,5 @@
 export {default as User} from './User'
+export {default as Role} from './Role'
 export {default as Media} from './Media'
 export {default as Cuisine} from './Cuisine'
 export {default as Product} from './Product'
