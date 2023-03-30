f72b232f529a4102b1e01b8e5940816ac886c699
Yash Pal <iyashpal.dev@gmail.com>
2023-03-30 18:30:59 +0530

    chore(fix) Fix the mis-spelled tag name.

--- resources/react/components/Index/Td.tsx
+++ resources/react/components/Index/Td.tsx
@@ -1,7 +1,7 @@
 export default function Td(props) {
   return <>
-    <th colSpan={props.colSpan} className={['whitespace-nowrap py-3 px-3 text-sm font-medium', props.className].join(' ')}>
+    <td colSpan={props.colSpan} className={['whitespace-nowrap py-3 px-3 text-sm font-medium', props.className].join(' ')}>
       {props.children}
-    </th>
+    </td>
   </>
 }
