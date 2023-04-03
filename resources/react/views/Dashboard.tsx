9452c06967d1fea2c59448556452e842c64248c6
Yash Pal <iyashpal.dev@gmail.com>
2023-04-03 17:50:08 +0530

    chore(new) Dashboard page static design added.

--- resources/react/views/Dashboard.tsx
+++ resources/react/views/Dashboard.tsx
@@ -4,7 +4,6 @@ import Breadcrumb from "~/layouts/AuthLayout/Breadcrumb";
 
 export default function Dashboard() {
   const {user} = useSelector(state => state.authSlice)
-  console.log(user)
   return <>
     <div className="">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
