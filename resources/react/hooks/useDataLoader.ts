60e3701d70b5d9e3bcf7bc58c66452ba4327ab0f
Yash Pal <iyashpal.dev@gmail.com>
2023-04-07 18:48:52 +0530

    chore(new) Dataloader and pagination hook added.

---
+++ resources/react/hooks/useDataLoader.ts
@@ -0,0 +1,42 @@
+import {Axios} from '@/helpers'
+import {useEffect, useState} from 'react'
+
+export default function useDataLoader<T extends unknown>(url: string, config = {}) {
+  const [data, setData] = useState<T>({} as T)
+  const [isProcessed, setIsProcessed] = useState<boolean>(false)
+  const [isProcessing, setIsProcessing] = useState<boolean>(false)
+
+  useEffect(() => {
+    sync()
+  }, [])
+
+  function sync(options = {}) {
+    setIsProcessing(true)
+    return Axios()
+      .get(url, {...config, ...options})
+      .then(handleSuccess)
+      .catch(handleError)
+      .finally(handleComplete)
+  }
+
+  function handleSuccess(axiosResponse) {
+    setData(axiosResponse.data)
+    return Promise.resolve(axiosResponse)
+  }
+
+  function handleError(axiosError) {
+    return Promise.reject(axiosError)
+  }
+
+  function handleComplete() {
+    setIsProcessed(true)
+    setIsProcessing(false)
+  }
+
+  return {
+    sync,
+    response: data,
+    isProcessed: () => isProcessed,
+    isProcessing: () => isProcessing,
+  }
+}
