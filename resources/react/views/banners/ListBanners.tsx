e57d55e61266cf4648cb8858902f206fc305adf2
Yash Pal <iyashpal.dev@gmail.com>
2023-04-14 15:33:31 +0530

    chore(refactor) Banner list page react imports optimized.

--- resources/react/views/banners/ListBanners.tsx
+++ resources/react/views/banners/ListBanners.tsx
@@ -1,15 +1,28 @@
+import Icons from '~/helpers/icons'
 import {useDataLoader} from "@/hooks";
 import {Banner} from "@/types/models";
 import {useEffect, useState} from "react";
+import * as Alert from "@/components/alerts";
 import {PaginatorMeta} from "@/types/paginators";
 import Pagination from "@/components/Pagination";
 import TrashModal from "@/components/TrashModal";
-import {Link, useLocation, useNavigate, useSearchParams} from "react-router-dom";
-import ListTable from "@/components/page/ListTable";
-import ListFilters from "@/components/page/ListFilters";
+import {ListFilters, ListTable} from "@/components/page";
 import Breadcrumb from "~/layouts/AuthLayout/Breadcrumb";
-import {BookmarkIcon, EyeIcon, HashtagIcon, PencilSquareIcon, TrashIcon} from "@heroicons/react/24/outline";
-import * as Alert from "@/components/alerts";
+import {Link, useLocation, useNavigate, useSearchParams} from "react-router-dom";
+
+const columns = [
+  {name: 'id', options: {className: 'text-center'}},
+  {name: 'Name', options: {className: 'text-left'}},
+  {name: 'Group', options: {className: 'text-left'}},
+  {name: 'Type', options: {className: 'text-left'}},
+  {name: 'Update Date', options: {className: 'text-left'}},
+  {name: 'status', options: {className: 'text-left'}},
+]
+
+const sortByFilters = [
+  {label: 'ID', value: 'id', icon: <Icons.Outline.Hashtag className={'w-5 h-5'}/>},
+  {label: 'Name', value: 'name', icon: <Icons.Outline.Bookmark className={'w-5 h-5'}/>}
+]
 
 export default function ListBanners() {
   const location = useLocation()
@@ -19,14 +32,6 @@ export default function ListBanners() {
   const [isTrashing, setIsTrashing] = useState<boolean>(false)
   const loader = useDataLoader<{ data: Banner[], meta: PaginatorMeta }>(`/banners`)
 
-  const columns = {
-    id: {name: 'id', options: {className: 'text-center'}},
-    product_name: {name: 'Name', options: {className: 'text-left'}},
-    description: {name: 'Group', options: {className: 'text-left'}},
-    sku: {name: 'Type', options: {className: 'text-left'}},
-    updated_at: {name: 'Update Date', options: {className: 'text-left'}},
-    status: {name: 'status', options: {className: 'text-left'}},
-  }
 
   useEffect(() => {
     loader.sync({params: {page: searchParams.get('page') ?? 1}})
@@ -58,31 +63,29 @@ export default function ListBanners() {
         <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mt-5">
           <div className={``}>
 
-            <ListFilters
-              create={{url: '/app/banners/create', label: 'Add Banner'}}
-              sortBy={[
-                {label: 'ID', value: 'id', icon: <HashtagIcon className={'w-5 h-5'}/>},
-                {label: 'Name', value: 'name', icon: <BookmarkIcon className={'w-5 h-5'}/>}
-              ]}
-            />
+            <ListFilters sortBy={sortByFilters} create={{url: '/app/banners/create', label: 'Add Banner'}}/>
 
             <ListTable
-              thead={Object.values(columns)}
+              thead={columns}
               tbody={loader.response.data.map(banner => {
                 return [
-                  banner.id, banner.title, banner.options.page, banner.options.type, banner.updated_at,
+                  banner.id,
+                  banner.title,
+                  banner.options.page,
+                  banner.options.type,
+                  banner.updated_at,
                   banner.status === 'active' ? <span className={`font-semibold uppercase text-red-primary`}>Active</span> : <span className={`font-semibold uppercase`}>In-Active</span>,
                   <div className="flex item-center justify-center gap-x-1">
                     <Link to={`/app/banners/${banner.id}/edit`} className={'bg-gray-100 border border-gray-400 text-gray-500 rounded-lg p-1 hover:border-blue-700 hover:bg-blue-100 hover:text-blue-700 transition-colors ease-in-out duration-300'}>
-                      <PencilSquareIcon className={'w-5 h-5'}/>
+                      <Icons.Outline.PencilSquare className={'w-5 h-5'}/>
                     </Link>
 
                     <Link to={`/app/banners/${banner.id}`} className={'bg-gray-100 border border-gray-400 text-gray-500 rounded-lg p-1 hover:border-green-700 hover:bg-green-100 hover:text-green-700 transition-colors ease-in-out duration-300'}>
-                      <EyeIcon className={'w-5 h-5'}/>
+                      <Icons.Outline.Eye className={'w-5 h-5'}/>
                     </Link>
 
                     <button onClick={() => toggleTrash(banner)} className={'bg-gray-100 border border-gray-400 text-gray-500 rounded-lg p-1 hover:border-red-700 hover:bg-red-100 hover:text-red-700 transition-colors ease-in-out duration-300'}>
-                      <TrashIcon className={'w-5 h-5'}/>
+                      <Icons.Outline.Trash className={'w-5 h-5'}/>
                     </button>
                   </div>
                 ]
