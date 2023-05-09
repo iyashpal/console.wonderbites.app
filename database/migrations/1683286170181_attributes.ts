5e2abc9dd4a0c22656ebb9ae61be5e9eb015c296
Yash Pal <iyashpal.dev@gmail.com>
2023-05-09 12:38:59 +0530

    chore(fix) Increment datatype field fixes.

--- database/migrations/1683286170181_attributes.ts
+++ database/migrations/1683286170181_attributes.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.bigIncrements('id')
       table.string('name')
       table.text('description').nullable()
       table.string('price').defaultTo(0)
--- database/migrations/Pivot/1683287234646_attribute_variant.ts
+++ database/migrations/Pivot/1683287234646_attribute_variant.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.bigIncrements('id')
       table.bigInteger('attribute_id').unsigned().references('attributes.id').onDelete('CASCADE')
-      table.bigInteger('variant_id').unsigned().references('variant.id').onDelete('CASCADE')
+      table.bigInteger('variant_id').unsigned().references('variants.id').onDelete('CASCADE')
       table.string('quantity').nullable().defaultTo(1)
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
