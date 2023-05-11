37758e80fa0b75f92741691e7a46323aff19a16c
Yash Pal <iyashpal.dev@gmail.com>
2023-05-11 19:26:22 +0530

    chore(new) Resource List component utilized on ingredients list page.

--- resources/react/pages/ingredients/ListIngredients.tsx
+++ resources/react/pages/ingredients/ListIngredients.tsx
@@ -1,30 +1,29 @@
 import Skeleton from './skeleton'
 import Icons from '~/helpers/icons'
-import {useEffect, useState} from 'react'
-import {Ingredient} from '~/contracts/schema'
+import { useEffect, useState } from 'react'
 import * as Alert from '~/components/alerts'
-import {useDataLoader, useFlash} from '@/hooks'
-import Pagination from '~/components/Pagination'
+import { Ingredient } from '~/contracts/schema'
+import { useDataLoader, useFlash } from '@/hooks'
 import TrashModal from '@/components/TrashModal'
-import {MetaData} from '@/contracts/pagination'
+import { MetaData } from '@/contracts/pagination'
+import { ResourceList } from '@/components/resources'
 import Breadcrumb from '~/layouts/AuthLayout/Breadcrumb'
-import {ListFilters, ListTable} from "@/components/page";
-import {Link, useLocation, useNavigate, useSearchParams} from 'react-router-dom'
+import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 
 const columns = [
-  {name: 'ID', options: {className: 'text-center'}},
-  {name: 'Ingredient Name', options: {className: 'text-left'}},
-  {name: 'UoM', options: {className: 'text-left'}},
-  {name: 'QTY', options: {className: 'text-center'}},
-  {name: 'MIN QTY', options: {className: 'text-center'}},
-  {name: 'MAX QTY', options: {className: 'text-center'}},
-  {name: 'Price', options: {className: 'text-center'}},
-  {name: 'Category', options: {className: 'text-center'}},
-  {name: 'Image', options: {className: 'text-center'}},
+  { name: 'ID', options: { className: 'text-center' } },
+  { name: 'Ingredient Name', options: { className: 'text-left' } },
+  { name: 'UoM', options: { className: 'text-left' } },
+  { name: 'QTY', options: { className: 'text-center' } },
+  { name: 'MIN QTY', options: { className: 'text-center' } },
+  { name: 'MAX QTY', options: { className: 'text-center' } },
+  { name: 'Price', options: { className: 'text-center' } },
+  { name: 'Category', options: { className: 'text-center' } },
+  { name: 'Image', options: { className: 'text-center' } },
 ]
 const sortByFilters = [
-  {label: 'ID', value: 'id', icon: <Icons.Outline.Hashtag className={'w-5 h-5'}/>},
-  {label: 'Name', value: 'name', icon: <Icons.Outline.Bookmark className={'w-5 h-5'}/>}
+  { label: 'ID', value: 'id', icon: <Icons.Outline.Hashtag className={'w-5 h-5'} /> },
+  { label: 'Name', value: 'name', icon: <Icons.Outline.Bookmark className={'w-5 h-5'} /> }
 ]
 
 export default function ListIngredients() {
@@ -38,7 +37,7 @@ export default function ListIngredients() {
   const [isTrashing, setIsTrashing] = useState<boolean>(false)
 
   useEffect(() => {
-    loader.sync({params: {page: searchParams.get('page') ?? 1}})
+    loader.sync({ params: { page: searchParams.get('page') ?? 1 } })
   }, [location, searchParams])
 
   function onDelete() {
@@ -70,7 +69,7 @@ export default function ListIngredients() {
       <>
         <div className="py-6">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-            <Breadcrumb pages={[{name: 'Ingredients'}]}/>
+            <Breadcrumb pages={[{ name: 'Ingredients' }]} />
           </div>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mt-5">
 
@@ -78,12 +77,14 @@ export default function ListIngredients() {
               <Alert.Success className={'mb-6'}>Ingredient deleted successfully</Alert.Success>
             </>}
 
-            <ListFilters sortBy={sortByFilters} create={{url: '/app/ingredients/create', label: 'Add Ingredient'}}/>
-
-            <ListTable
-              thead={columns}
-
-              tbody={loader.response.data.map(ingredient => ([
+            <ResourceList
+              columns={columns}
+              sorting={sortByFilters}
+              data={loader.response.data}
+              metadata={loader.response.meta}
+              createLink={{ label: 'Add Ingredient', href: '/app/ingredients/create' }}
+            >
+              {({ data }) => data.map(ingredient => ([
                 ingredient.id,
                 <Link to={`/app/ingredients/${ingredient.id}`} className={'text-red-primary'}> {ingredient.name} </Link>,
                 ingredient.unit,
@@ -92,31 +93,21 @@ export default function ListIngredients() {
                 ingredient.maxQuantity,
                 `${ingredient.price}L`,
                 ingredientCategory(ingredient),
-                <img className={'w-9 h-9 rounded-full object-cover mx-auto border-2 shadow'} src={ingredient.thumbnailUrl} alt={ingredient.name}/>,
+                <img className={'w-9 h-9 rounded-full object-cover mx-auto border-2 shadow'} src={ingredient.thumbnailUrl} alt={ingredient.name} />,
                 <div className="flex item-center justify-center gap-x-1">
                   <Link to={`/app/ingredients/${ingredient.id}/edit`} className={'action:button button:blue'}>
-                    <Icons.Outline.PencilSquare className={'w-5 h-5'}/>
+                    <Icons.Outline.PencilSquare className={'w-5 h-5'} />
                   </Link>
 
                   <Link to={`/app/ingredients/${ingredient.id}`} className={'action:button button:green'}>
-                    <Icons.Outline.Eye className={'w-5 h-5'}/>
+                    <Icons.Outline.Eye className={'w-5 h-5'} />
                   </Link>
                   <button onClick={() => toggleTrash(ingredient)} className={'action:button button:red'}>
-                    <Icons.Outline.Trash className={'w-5 h-5'}/>
+                    <Icons.Outline.Trash className={'w-5 h-5'} />
                   </button>
                 </div>
               ]))}
-
-              empty={(
-                <Alert.Warning>
-                  No ingredients available.{' '}
-                  <Link to={'/app/ingredients/create'} className="font-medium text-yellow-700 underline hover:text-yellow-600">
-                    Click here to add more ingredients.
-                  </Link>
-                </Alert.Warning>
-              )}/>
-
-            <Pagination meta={loader.response.meta}/>
+            </ResourceList>
           </div>
         </div>
 
@@ -130,6 +121,6 @@ export default function ListIngredients() {
         />
       </>
 
-      : <Skeleton.List.Page/>}
+      : <Skeleton.List.Page />}
   </>
 }
