e55cc371a702cdc9a8763a4667175a8398686bef
Yash Pal <iyashpal.dev@gmail.com>
2023-05-11 18:42:11 +0530

    chore(new) New resource list component utilized.

--- resources/react/pages/users/ListUsers.tsx
+++ resources/react/pages/users/ListUsers.tsx
@@ -1,30 +1,21 @@
-import {DateTime} from 'luxon'
+import { DateTime } from 'luxon'
 import Icons from '~/helpers/icons'
-import {User} from '~/contracts/schema'
-import {useEffect, useState} from 'react'
-import * as Alert from '@/components/alerts'
-import {MetaData} from '@/contracts/pagination'
-import Pagination from '@/components/Pagination'
+import { User } from '~/contracts/schema'
+import { useEffect, useState } from 'react'
 import TrashModal from '@/components/TrashModal'
+import { MetaData } from '@/contracts/pagination'
+import { ResourceList } from '@/components/resources'
 import Breadcrumb from '~/layouts/AuthLayout/Breadcrumb'
-import {ListFilters, ListTable} from '@/components/page'
-import {useDataLoader, useFlash, useSelector} from '@/hooks'
-import {Link, useLocation, useNavigate, useSearchParams} from 'react-router-dom'
+import { useDataLoader, useFlash, useSelector } from '@/hooks'
+import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 
 const columns = [
-  {name: 'ID', options: {className: 'text-center'}},
-  {name: 'Name', options: {className: 'text-left'}},
-  {name: 'Role', options: {className: 'text-left'}},
-  {name: 'Email', options: {className: 'text-left'}},
-  {name: 'Created On', options: {className: 'text-left'}},
-  {name: 'status', options: {className: 'text-left'}},
-]
-
-const sortByFilters = [
-  {label: 'ID', value: 'id', icon: <Icons.Outline.Hashtag className="h-5 w-5" aria-hidden="true"/>},
-  {label: 'Date', value: 'date', icon: <Icons.Outline.CalendarDays className="h-5 w-5" aria-hidden="true"/>},
-  {label: 'Name', value: 'name', icon: <Icons.Outline.Bookmark className="h-5 w-5" aria-hidden="true"/>},
-  {label: 'Email', value: 'email', icon: <Icons.Outline.Envelope className="h-5 w-5" aria-hidden="true"/>},
+  { name: 'ID', options: { className: 'text-center' } },
+  { name: 'Name', options: { className: 'text-left' } },
+  { name: 'Role', options: { className: 'text-left' } },
+  { name: 'Email', options: { className: 'text-left' } },
+  { name: 'Created On', options: { className: 'text-left' } },
+  { name: 'status', options: { className: 'text-left' } },
 ]
 
 export default function ListUsers() {
@@ -32,14 +23,14 @@ export default function ListUsers() {
   const location = useLocation()
   const navigateTo = useNavigate()
   const [searchParams] = useSearchParams()
+  const { user: authUser } = useSelector(state => state.authSlice)
   const loader = useDataLoader<{ data: User[], meta: MetaData }>(`/users`)
-  const {user: authUser} = useSelector(state => state.authSlice)
 
   const [user, setUser] = useState<User>({} as User)
   const [isTrashing, setIsTrashing] = useState<boolean>(false)
 
   useEffect(() => {
-    loader.sync({params: {page: searchParams.get('page') ?? 1}})
+    loader.sync({ params: { page: searchParams.get('page') ?? 1 } })
   }, [searchParams, location])
 
   function toggleTrash(o: User) {
@@ -62,52 +53,52 @@ export default function ListUsers() {
     {loader.isProcessed() && (
       <div className="py-6">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-          <Breadcrumb pages={[{name: 'Users'}]}/>
+          <Breadcrumb pages={[{ name: 'Users' }]} />
         </div>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mt-5 flex flex-col">
-          <ListFilters sortBy={sortByFilters} create={{url: '/app/users/create', label: 'Create User'}}/>
+          <ResourceList
+            columns={columns}
+            data={loader.response.data}
 
-          <ListTable
-            thead={columns}
-            tbody={loader.response.data.map(user => ([
-              user.id,
-              user.name,
-              user.role?.title,
-              user.email,
-              user.created_at ? DateTime.fromISO(user.created_at).toLocaleString(DateTime.DATETIME_SHORT) : '-',
-              user.status === 1 ? 'Active' : 'In-active',
-              <div className="flex item-center justify-center gap-x-1">
-                <Link to={`/app/users/${user.id}/edit`} className={'action:button button:blue'}>
-                  <Icons.Outline.PencilSquare className={'w-5 h-5'}/>
-                </Link>
+            sorting={[
+              { label: 'ID', value: 'id' },
+              { label: 'Name', value: 'name' },
+              { label: 'Email', value: 'email' },
+            ]}
+            metadata={loader.response.meta}
+            createLink={{ label: 'Create new', href: '/app/users/create' }}
+          >
+            {({ data }) => (
+              data.map(user => ([
+                user.id,
+                user.name,
+                user.role?.title,
+                user.email,
+                user.created_at ? DateTime.fromISO(user.created_at).toLocaleString(DateTime.DATETIME_SHORT) : '-',
+                user.status === 1 ? 'Active' : 'In-active',
+                <div className="flex item-center justify-center gap-x-1">
+                  <Link to={`/app/users/${user.id}/edit`} className={'action:button button:blue'}>
+                    <Icons.Outline.PencilSquare className={'w-5 h-5'} />
+                  </Link>
 
-                <Link to={`/app/users/${user.id}`} className={'action:button button:green'}>
-                  <Icons.Outline.Eye className={'w-5 h-5'}/>
-                </Link>
+                  <Link to={`/app/users/${user.id}`} className={'action:button button:green'}>
+                    <Icons.Outline.Eye className={'w-5 h-5'} />
+                  </Link>
 
-                {authUser?.id !== user.id ? (
-                  <button onClick={() => toggleTrash(user)} className={'action:button button:red'}>
-                    <Icons.Outline.Trash className={'w-5 h-5'}/>
-                  </button>
-                ) : (
-                  <button disabled className={'action:button button:disabled'}>
-                    <Icons.Outline.Trash className={'w-5 h-5'}/>
-                  </button>
-                )}
-              </div>
-            ]))}
-
-            empty={(
-              <Alert.Warning>
-                No users available.{' '}
-                <Link to={'/app/users/create'} className="font-medium text-yellow-700 underline hover:text-yellow-600">
-                  Click here to add more users.
-                </Link>
-              </Alert.Warning>
+                  {authUser?.id !== user.id ? (
+                    <button onClick={() => toggleTrash(user)} className={'action:button button:red'}>
+                      <Icons.Outline.Trash className={'w-5 h-5'} />
+                    </button>
+                  ) : (
+                    <button disabled className={'action:button button:disabled'}>
+                      <Icons.Outline.Trash className={'w-5 h-5'} />
+                    </button>
+                  )}
+                </div>
+              ]))
             )}
-          />
+          </ResourceList>
 
-          <Pagination meta={loader.response.meta}/>
         </div>
       </div>
     )}
