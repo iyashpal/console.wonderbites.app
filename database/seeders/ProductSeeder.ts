18c9fc107dc986839319a03daf76bfb1e800ea4b
Yash Pal <iyashpal.dev@gmail.com>
2023-05-11 12:07:34 +0530

    chore(update) Product type added to product seeder.

--- database/seeders/ProductSeeder.ts
+++ database/seeders/ProductSeeder.ts
@@ -3,7 +3,6 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 
 export default class ProductSeederSeeder extends BaseSeeder {
   public async run () {
-    //
     const user = await User.updateOrCreate({email: 'yash@brandsonify.com'}, {
       firstName: 'Yash',
       lastName: 'Pal',
@@ -15,72 +14,65 @@ export default class ProductSeederSeeder extends BaseSeeder {
       {
         userId: user.id,
         name: 'Salmon Sallad',
-        description: `The beauty of this Salad Bowl is that it is completely customizable.
-        The baked salmon with homemade teriyaki sauce is the star of the show but you can choose how to showcase it.
-        Swap the spinach/carrots/edamame for any of your favorite veggies`,
+        description: 'The beauty of this Salad Bowl is that it is completely customizable.',
         price: 500,
         calories: '452',
         sku: '001',
+        type: 'general',
       },
       {
         userId: user.id,
         name: 'Chicken Soup Curry',
-        description: `The beauty of this Salad Bowl is that it is completely customizable.
-        The baked salmon with homemade teriyaki sauce is the star of the show but you can choose how to showcase it.
-        Swap the spinach/carrots/edamame for any of your favorite veggies`,
+        description: 'The beauty of this Salad Bowl is that it is completely customizable.',
         price: 500,
         calories: '453',
         sku: '002',
+        type: 'general',
       },
       {
         userId: user.id,
         name: 'Pizza Margharita',
-        description: `The beauty of this Salad Bowl is that it is completely customizable.
-        The baked salmon with homemade teriyaki sauce is the star of the show but you can choose how to showcase it.
-        Swap the spinach/carrots/edamame for any of your favorite veggies`,
+        description: 'The beauty of this Salad Bowl is that it is completely customizable.',
         price: 500,
         calories: '454',
         sku: '003',
+        type: 'general',
       },
       {
         userId: user.id,
         name: 'Shrimps Soup',
-        description: `The beauty of this Salad Bowl is that it is completely customizable.
-        The baked salmon with homemade teriyaki sauce is the star of the show but you can choose how to showcase it.
-        Swap the spinach/carrots/edamame for any of your favorite veggies`,
+        description: 'The beauty of this Salad Bowl is that it is completely customizable.',
         price: 500,
         calories: '455',
         sku: '004',
+        type: 'general',
       },
       {
         userId: user.id,
         name: 'Bacon Burger',
-        description: `The beauty of this Salad Bowl is that it is completely customizable.
-        The baked salmon with homemade teriyaki sauce is the star of the show but you can choose how to showcase it.
-        Swap the spinach/carrots/edamame for any of your favorite veggies`,
+        description: 'The beauty of this Salad Bowl is that it is completely customizable.',
         price: 500,
         calories: '455',
         sku: '004',
+        type: 'general',
       },
       {
         userId: user.id,
         name: 'Salad Bowl',
-        description: `The beauty of this Salad Bowl is that it is completely customizable.
-        The baked salmon with homemade teriyaki sauce is the star of the show but you can choose how to showcase it.
-        Swap the spinach/carrots/edamame for any of your favorite veggies`,
+        description: 'The beauty of this Salad Bowl is that it is completely customizable.',
         price: 500,
         calories: '455',
         sku: '004',
+        type: 'general',
       },
       {
         userId: user.id,
         name: 'Coca Cola',
-        description: `The beauty of this Salad Bowl is that it is completely customizable.
-        The baked salmon with homemade teriyaki sauce is the star of the show but you can choose how to showcase it.
-        Swap the spinach/carrots/edamame for any of your favorite veggies`,
+        description: 'The beauty of this Salad Bowl is that it is completely customizable.',
         price: 500,
         calories: '455',
         sku: '004',
+        type: 'general',
       },
     ])
   }
