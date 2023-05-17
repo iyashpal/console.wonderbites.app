c0e69ce909c3da51de6e513c81024f50f2496bfa
Yash Pal <iyashpal.dev@gmail.com>
2023-05-17 19:44:27 +0530

    chore(new) Type safe form hook added.

---
+++ resources/react/hooks/forms/useProductVariantForm.ts
@@ -0,0 +1,31 @@
+import { Axios } from "@/helpers"
+import { useState } from "react"
+
+type ErrorsType<U> = { [key in keyof U]?: string }
+
+export default function useProductVariantForm<T>() {
+    const [form, setForm] = useState({} as T)
+    const [errors, setErrors] = useState<ErrorsType<T>>({} as ErrorsType<T>)
+
+    function sync(data: T) {
+        setForm(data)
+    }
+
+
+    function create() {
+        return Axios()
+            .post(`variants`, form)
+            .then(() => {
+                return Promise.resolve()
+            }).catch(({ response }) => {
+                setErrors(response?.data?.errors ?? {})
+                return Promise.reject()
+            })
+    }
+
+    function update() {
+
+    }
+
+    return { sync, create, update, errors, data: form }
+}
