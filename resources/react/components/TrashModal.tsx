7acd64b60201120a143852e40581f10f9ef1f8bd
Yash Pal <iyashpal.dev@gmail.com>
2023-05-22 19:20:31 +0530

    chore(new) Trash modal UI updated.

--- resources/react/components/TrashModal.tsx
+++ resources/react/components/TrashModal.tsx
@@ -1,9 +1,9 @@
 import { Axios } from "@/helpers";
+import Icons from "@/helpers/icons";
 import { AxiosResponse } from "axios";
 import { Fragment, useState } from "react";
 import * as Loaders from '@/components/loaders';
 import { Dialog, Transition } from "@headlessui/react";
-import { TrashIcon, ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 type TrashProps = {
   url: string,
@@ -37,7 +37,7 @@ export default function TrashModal({ show = false, request, onClose, onDelete, t
 
   function handle() {
     if (request) {
-     return Axios()[request.type ?? 'delete'](url, request.data) 
+      return Axios()[request.type ?? 'delete'](url, request.data)
     }
 
     return Axios().delete(url)
@@ -69,13 +69,20 @@ export default function TrashModal({ show = false, request, onClose, onDelete, t
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md sm:p-6">
+                <div className="flex items-start justify-between">
+                  <div className="mx-auto flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-red-100 outline outline-8 outline-red-50 sm:mx-0 sm:h-10 sm:w-10">
+                    <Icons.Outline.Trash className="h-6 w-6 text-red-600" aria-hidden="true" />
                   </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                  <div>
+                    <button onClick={onClose}>
+                      <Icons.Outline.XMark className="h-6 w-6" />
+                    </button>
+                  </div>
+                </div>
+                <div className="sm:flex sm:items-start pt-3">
+                  <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                    <Dialog.Title as="h3" className="text-lg font-semibold leading-6 text-gray-900">
                       {title}
                     </Dialog.Title>
                     <div className="mt-2">
@@ -86,14 +93,14 @@ export default function TrashModal({ show = false, request, onClose, onDelete, t
                   </div>
                 </div>
                 <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                  <button type="button" className="inline-flex w-full items-center justify-center gap-x-1 rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm" onClick={onSubmit}>
+                  <button type="button" className="flex-auto flex w-full items-center justify-center gap-x-1 rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm" onClick={onSubmit}>
                     {isProcessing ? <>
                       <Loaders.Circle className={'animate-spin h-4 w-4'} /> Deleting
                     </> : <>
-                      <TrashIcon className={'w-4 h-4'} /> Delete
+                      <Icons.Outline.Trash className={'w-4 h-4'} /> Delete
                     </>}
                   </button>
-                  <button type="button" className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm" onClick={onClose}>
+                  <button type="button" className="flex-auto flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm" onClick={onClose}>
                     Cancel
                   </button>
                 </div>
