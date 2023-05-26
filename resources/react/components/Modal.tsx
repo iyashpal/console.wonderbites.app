14ea20cfc19d8d47435c64e3f2248f9e3b65ee4a
Yash Pal <iyashpal.dev@gmail.com>
2023-05-26 11:06:18 +0530

    chore(fix) Modal rounded corner removed.

--- resources/react/components/Modal.tsx
+++ resources/react/components/Modal.tsx
@@ -23,7 +23,7 @@ export default function Modal({show = false, onClose, children, className = ''}:
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4">
             <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
-              <Dialog.Panel {...classNames('w-full relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all', className)}>
+              <Dialog.Panel {...classNames('w-full relative transform overflow-hidden bg-white shadow-xl transition-all', className)}>
                 {children}
               </Dialog.Panel>
             </Transition.Child>
