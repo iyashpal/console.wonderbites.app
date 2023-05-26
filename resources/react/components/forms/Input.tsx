1cfd3baaa0300eeb001b00ebe53e789b49ec94b6
Yash Pal <iyashpal.dev@gmail.com>
2023-05-26 19:34:24 +0530

    chore(new) Form input component added.

---
+++ resources/react/components/forms/Input.tsx
@@ -0,0 +1,34 @@
+import { classNames } from "@/helpers"
+import { useEffect, useState } from "react"
+
+type InputProps = React.ComponentProps<'input'> & {
+    override?: boolean
+    error?: string
+}
+
+export default function Input({ error, override, className, ...props }: InputProps) {
+    const [hasError, setHasError] = useState(!!error)
+    const [classList, setClassList] = useState<string[]>([])
+
+    useEffect(() => {
+        setHasError(!!error)
+    }, [error])
+
+    useEffect(() => {
+        setClassList([
+            (hasError ?
+                'text-red-500 border-red-200 focus:ring-red-200 focus:border-red-500 placeholder:text-red-500'
+                :
+                'text-gray-500 border-gray-200 focus:ring-gray-200 focus:border-gray-500 placeholder:text-gray-500'
+            )
+
+        ])
+    }, [hasError])
+
+    return (
+        <div className="relative">
+            <input {...props} className={classNames(override ? className : `w-full focus:border-gray-500 focus:ring-4`, classList)} />
+            {hasError && <p className="absolute capitalize text-red-500 text-xs pt-1">{error}</p>}
+        </div>
+    )
+}
