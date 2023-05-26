986d1e0d8d71f444f7e07d758c0ab47831e5df32
Yash Pal <iyashpal.dev@gmail.com>
2023-05-26 19:35:18 +0530

    chore(new) Global form hook updated with simple integration.

--- resources/react/hooks/useForm.ts
+++ resources/react/hooks/useForm.ts
@@ -1,13 +1,155 @@
-import {useState} from "react";
+import { useState } from "react";
+import { Axios } from "@/helpers";
+import { ErrorsType } from "@/contracts";
+import { AxiosRequestConfig } from "axios";
 
-export default function useForm<T>(fields) {
+export default function useForm<T>(initialState: T = {} as T) {
 
-  const [formFields] = useState(fields)
+  const [form, setForm] = useState(initialState)
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [errors, setErrors] = useState<ErrorsType<T>>({} as ErrorsType<T>)
 
-  const [errors] = useState<T>({} as T)
+  /**
+   * Handle the on change event for input fields.
+   *
+   * @param field string
+   * @returns {Function}
+   */
+  function onChange(field: keyof T) {
+    return (event) => {
+      setForm(data => ({ ...data, [field]: event.target.value }))
+    }
+  }
+
+  /**
+  * Synchronize the whole form data.
+  *
+  * @param data {Object}
+  * @returns void
+  */
+  const sync = (data: T): void => setForm(data)
+
+  /**
+  * Reset the form errors.
+  *
+  * @returns void
+  */
+  const resetErrors = (): void => setErrors({} as ErrorsType<T>)
+
+  /**
+  * Get the form error.
+  *
+  * @param key string
+  * @returns string | undefined
+  */
+  const error = (key: keyof T): undefined | string => errors[key]
+
+  /**
+  * Set a value to form input.
+  *
+  * @param key string
+  * @param value any
+  */
+  function set<V>(key: keyof T, value: V) {
+    setForm(e => ({ ...e, [key]: value }))
+  }
+
+  /**
+   * Get value by key.
+   *
+   * @param key string
+   * @param defaults any
+   * @returns any
+   */
+  function get<V>(key: keyof T, defaults: V = null as V) {
+    return form[key] ?? defaults
+  }
+
+  /**
+   * Handle the form get request.
+   *
+   * @param url string
+   * @param config AxiosRequestConfig<any>
+   * @returns Promise
+   */
+  const handleGet = (url: string, config?: AxiosRequestConfig<any>) => {
+    return request({ url, method: 'get', ...config })
+  }
+
+  /**
+   * Handle the form post request.
+   *
+   * @param url string
+   * @param config AxiosRequestConfig<any>
+   * @returns Promise
+   */
+  const handlePost = (url: string, config?: AxiosRequestConfig<any>) => {
+    return request({ url, method: 'post', data: form, ...config })
+  }
+
+  /**
+   * Handle the form put request.
+   *
+   * @param url string
+   * @param config AxiosRequestConfig<any>
+   * @returns Promise
+   */
+  const handlePut = (url: string, config?: AxiosRequestConfig<any>) => {
+    return request({ url, method: 'put', data: form, ...config })
+  }
+
+  /**
+   * Handle the form patch request.
+   *
+   * @param url string
+   * @param config AxiosRequestConfig<any>
+   * @returns Promise
+   */
+  const handlePatch = (url: string, config?: AxiosRequestConfig<any>) => {
+    return request({ url, method: 'patch', data: form, ...config })
+  }
+
+  /**
+   * Handle the form delete request.
+   *
+   * @param url string
+   * @param config AxiosRequestConfig<any>
+   * @returns Promise
+   */
+  const handleDelete = (url: string, config?: AxiosRequestConfig<any>) => {
+    return request({ url, method: 'delete', ...config })
+  }
+
+  /**
+   * Handle the form request.
+   *
+   * @param config AxiosRequestConfig<any>
+   * @returns Promise
+   */
+  function request(config: AxiosRequestConfig<any>) {
+    resetErrors()
+    setIsProcessing(true)
+    return Axios().request(config).then(response => {
+      setIsProcessing(false)
+      return Promise.resolve(response)
+    }).catch(error => {
+      setIsProcessing(false)
+      setErrors(error.response.data?.errors ?? {})
+      return Promise.reject(error)
+    })
+  }
 
   return {
-    errors,
-    fields: formFields,
+    sync, set, get, onChange, data: form,
+    isProcessing,
+    errors: error,
+    handle: {
+      get: handleGet,
+      put: handlePut,
+      post: handlePost,
+      patch: handlePatch,
+      delete: handleDelete
+    },
+    reset: { errors: resetErrors },
   }
 }
