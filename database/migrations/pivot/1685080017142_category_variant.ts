f3d5285bdb01752b2bb48229bd2c7c933e2ebf83
Yash Pal <iyashpal.dev@gmail.com>
2023-05-29 19:18:05 +0530

    chore(new) relation field types updated.

--- database/migrations/pivot/1685080017142_category_variant.ts
+++ database/migrations/pivot/1685080017142_category_variant.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.bigInteger('category_id').references('categories.id').onDelete('CASCADE')
-      table.bigInteger('variant_id').references('variants.id').onDelete('CASCADE')
+      table.bigInteger('category_id').unsigned().references('categories.id').onDelete('CASCADE')
+      table.bigInteger('variant_id').unsigned().references('variants.id').onDelete('CASCADE')
       table.integer('order').nullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
