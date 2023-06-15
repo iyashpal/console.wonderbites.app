fce36052a530b23fa413f64d0a17c05303b60074
Yash Pal <iyashpal.dev@gmail.com>
2023-06-15 19:04:30 +0530

    chore(new) Variations added to form input field component.

--- resources/react/components/forms/Input.tsx
+++ resources/react/components/forms/Input.tsx
@@ -2,11 +2,20 @@ import { classNames } from "@/helpers"
 import { useEffect, useState } from "react"
 
 type InputProps = React.ComponentProps<'input'> & {
+    variant?: string
     override?: boolean
     error?: string
 }
 
-export default function Input({ error, override, className, ...props }: InputProps) {
+export default function Input({ variant, ...props }: InputProps) {
+    if (variant === 'underlined') {
+        return <Underlined {...props} />
+    }
+
+    return <Basic {...props} />
+}
+
+function Basic({ error, override, className, ...props }: InputProps) {
     const [hasError, setHasError] = useState(!!error)
     const [classList, setClassList] = useState<string[]>([])
 
@@ -21,13 +30,35 @@ export default function Input({ error, override, className, ...props }: InputPro
                 :
                 'text-gray-500 border-gray-200 focus:ring-gray-200 focus:border-gray-500 placeholder:text-gray-500'
             )
-
         ])
     }, [hasError])
 
     return (
         <div className="relative">
             <input {...props} className={classNames(override ? className : `focus:border-gray-500 focus:ring-4 ${className}`, classList)} />
+            {hasError && <p className="absolute capitalize text-red-500 text-xs pt-1">{error}</p>}
+        </div>
+    )
+}
+
+function Underlined({ error, override, className, ...props }: InputProps) {
+    const [hasError, setHasError] = useState(!!error)
+    const [classList, setClassList] = useState<string[]>([])
+
+    useEffect(() => {
+        setHasError(!!error)
+    }, [error])
+
+    useEffect(() => {
+        setClassList([
+            (hasError ? 'focus-within:bg-red-50 text-red-600 placeholder:text-red-500' : 'focus-within:bg-gray-50 text-gray-900')
+        ])
+    }, [hasError])
+
+    return (
+        <div className="relative">
+            <input {...props} className={classNames(override ? className : `peer block w-full border-0 py-1.5 focus:ring-0 sm:text-sm sm:leading-6 ${className}`, classList)} />
+            <div className={`absolute inset-x-0 bottom-0 border-t peer-focus:border-t-2 ${hasError ? 'border-red-300 peer-focus:border-red-600' : 'border-gray-300 peer-focus:border-slate-600'}`} aria-hidden="true" />
             {hasError && <p className="absolute capitalize text-red-500 text-xs pt-1">{error}</p>}
         </div>
     )
