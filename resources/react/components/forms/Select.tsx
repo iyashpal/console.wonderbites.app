1491c45901a02a3902fc57b838b567c7c61f3c81
Yash Pal <iyashpal.dev@gmail.com>
2023-06-21 17:06:57 +0530

    chore(new) New form components added.

--- resources/react/components/forms/Select.tsx
+++ resources/react/components/forms/Select.tsx
@@ -1,19 +1,20 @@
 import { classNames } from "@/helpers"
 import { useEffect, useState } from "react"
 
-type InputProps = React.ComponentProps<'input'> & {
+type InputProps = React.ComponentProps<'select'> & {
     variant?: 'underlined'
     override?: boolean
     error?: string
 }
+export default function Select({ variant, ...props }: InputProps) {
 
-export default function Input({ variant, ...props }: InputProps) {
     if (variant === 'underlined') {
         return <Underlined {...props} />
     }
 
     return <Basic {...props} />
 }
+
 
 function Basic({ error, override, className, ...props }: InputProps) {
     const [hasError, setHasError] = useState(!!error)
@@ -35,7 +36,10 @@ function Basic({ error, override, className, ...props }: InputProps) {
 
     return (
         <div className="relative">
-            <input {...props} className={classNames(override ? className : `focus:border-gray-500 focus:ring-4 ${className}`, classList)} />
+            <select {...props} className={classNames(override ? className : `focus:border-gray-500 focus:ring-4 ${className}`, classList)} >
+                {props.children}
+            </select>
+            {hasError && <p className="absolute capitalize text-red-500 text-xs pt-1">{error}</p>}
         </div>
     )
 }
@@ -56,7 +60,9 @@ function Underlined({ error, override, className, ...props }: InputProps) {
 
     return (
         <div className="relative">
-            <input {...props} className={classNames(override ? className : `peer block w-full border-0 py-1.5 focus:ring-0 sm:text-sm sm:leading-6 ${className}`, classList)} />
+            <select {...props} className={classNames(override ? className : `peer block w-full border-0 py-1.5 focus:ring-0 sm:text-sm sm:leading-6 ${className}`, classList)}>
+                {props.children}
+            </select>
             <div className={`absolute inset-x-0 bottom-0 border-t peer-focus:border-t-2 ${hasError ? 'border-red-300 peer-focus:border-red-600' : 'border-gray-300 peer-focus:border-slate-600'}`} aria-hidden="true" />
         </div>
     )
