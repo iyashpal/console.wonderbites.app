b95bd0b79e744778bd84747c0a0ab92d3ae5ea48
Yash Pal <iyashpal.dev@gmail.com>
2023-06-30 09:38:51 +0530

    chore(new) queue system package updated

--- config/queue.ts
+++ config/queue.ts
@@ -1,11 +1,11 @@
 import Env from '@ioc:Adonis/Core/Env'
-import type { QueueConfig } from '@ioc:Setten/Queue'
+import type {QueueConfig} from '@ioc:Rlanz/Queue'
 
 const queueConfig: QueueConfig = {
   connection: {
-    host: Env.get('REDIS_HOST'),
-    port: Env.get('REDIS_PORT'),
-    password: Env.get('REDIS_PASSWORD'),
+    host: Env.get('QUEUE_REDIS_HOST'),
+    port: Env.get('QUEUE_REDIS_PORT'),
+    password: Env.get('QUEUE_REDIS_PASSWORD'),
   },
 
   queue: {},
@@ -13,12 +13,37 @@ const queueConfig: QueueConfig = {
   worker: {},
 
   jobs: {
+    /*
+    |--------------------------------------------------------------------------
+    | Default Job Attempts
+    |--------------------------------------------------------------------------
+    |
+    | The default number of attempts after which the job will be marked as
+    | failed. You can also set the number of attempts on individual jobs
+    | as well.
+    |
+    | @see https://docs.bullmq.io/guide/retrying-failing-jobs
+    |
+    */
     attempts: 3,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Auto-Removal of Jobs
+    |--------------------------------------------------------------------------
+    |
+    | Numbers of jobs to keep in the completed and failed queues before they
+    | are removed. This is important to keep the size of these queues in
+    | control. Set the value to false to disable auto-removal.
+    |
+    | @see https://docs.bullmq.io/guide/queues/auto-removal-of-jobs
+    |
+    */
     removeOnComplete: 100,
     removeOnFail: 100,
   },
 }
 
-const queueNames:['default'] = ['default']
+const queueNames: ['default'] = ['default']
 
-export { queueConfig as config, queueNames}
+export {queueConfig as config, queueNames}
