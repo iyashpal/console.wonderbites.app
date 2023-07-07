6d47c6a26fa9c5d5b781e847024775df1ddcdb1b
iyashpal <iyashpal.dev@gmail.com>
2023-07-07 19:00:53 +0530

    chore(new) Order contract added to react codebase.

--- resources/react/contracts/schema/Order.ts
+++ resources/react/contracts/schema/Order.ts
@@ -1,9 +1,53 @@
 import User from "@/contracts/schema/User";
+import {Coupon} from "@/contracts/schema/index";
 
 export default interface Order {
-  id: number,
-  status:number,
-  created_at: string,
-  order_type: string,
-  user?: User
+    id: number,
+    user_id: number
+    coupon_id: number
+    token: string
+    order_type: string
+    payment_mode: string
+    first_name: string
+    last_name: string
+    street: string
+    city: string
+    phone: string
+    email: string
+    location: {
+        lat?: string
+        lng?: string
+    }
+    reserved_seats: number
+    eat_or_pickup_time: number
+    data: {
+        id: number
+        quantity: number
+        variant?: {
+            id: number
+            ingredients: {
+                id: number
+                quantity: number
+                category: number
+            }[]
+        }
+        ingredients?: {
+            id: number
+            quantity: number
+            category: number
+        }[]
+    }[]
+
+    option: {
+        extras: string[]
+        delivery_fee: string
+    }
+    channel: 'web' | 'app' | 'console'
+    note: string
+    status: string,
+    created_at: string,
+    updated_at: string
+
+    user?: User
+    coupon?: Coupon
 }
