86245dc09b85ca2e7e3908f06b84b43265f6929e
iyashpal <iyashpal.dev@gmail.com>
2023-07-11 18:32:40 +0530

    chore(update) Edit page is now using useForm hook to handle the form request.

--- resources/react/pages/banners/EditBanner.tsx
+++ resources/react/pages/banners/EditBanner.tsx
@@ -1,103 +1,141 @@
-import { useEffect } from "react";
-import { useDataLoader } from "@/hooks";
-import { Banner } from "~/contracts/schema";
-import { useParams } from "react-router-dom";
-import { useBannerForm } from "@/hooks/forms";
+import {useEffect} from "react";
+import {Banner} from "~/contracts/schema";
+import {useNavigate, useParams} from "react-router-dom";
 import Resources from "@/components/resources";
+import {useDataLoader, useForm} from "@/hooks";
 import InputError from "@/components/Form/InputError";
 import Breadcrumb from "@/layouts/AuthLayout/Breadcrumb";
 
 export default function CreateBanner() {
-
-  const { id } = useParams()
+  const navigateTo = useNavigate()
+  const {id} = useParams()
   const loader = useDataLoader<{ banner: Banner }>(`banners/${id}/edit`)
 
-  const form = useBannerForm({
-    title: '', status: 'active', options: { page: 'home', section: 'todayspick', type: 'image', link: '' },
-  })
+  const form = useForm<{
+    id: number,
+    title: string,
+    attachment: string,
+    status: string,
+    page: string,
+    section: string,
+    type: string,
+    link: string,
+  }>()
 
   useEffect(() => {
-    if (loader.isProcessed()) {
-      form.input.set('id', loader.response.banner.id)
-      form.input.set('title', loader.response.banner.title)
-      form.input.set('status', loader.response.banner.status)
-      form.input.set('options', loader.response.banner.options)
+    let banner = loader.response.banner
+    if (banner?.id) {
+      form.sync({
+        id: banner.id,
+        attachment: '',
+        title: banner.title,
+        status: banner.status,
+        page: banner.options.page,
+        type: banner.options.type,
+        link: banner.options.link,
+        section: banner.options.section,
+      })
     }
-  }, [loader])
+  }, [loader.response.banner])
+
+  function onSubmit(e) {
+    e.preventDefault()
+    form.put(`/banners/${form.input.id}`).then(({data}) => {
+      navigateTo(`/app/banners/${data.id}`)
+    })
+  }
 
   if (loader.isProcessed()) {
     return (
       <div className="py-6">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-          <Breadcrumb pages={[{ name: 'Banners', href: '/app/banners' }, { name: 'Edit Banner' }]} />
+          <Breadcrumb pages={[{name: 'Banners', href: '/app/banners'}, {name: 'Edit Banner'}]}/>
         </div>
 
         <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mt-5 flex flex-col">
 
-          <Resources.Form title="Edit Banner" backUrl="/app/banners" onSubmit={form.onSubmit.update} processing={form.isProcessing}>
+          <Resources.Form title="Edit Banner" backUrl="/app/banners" onSubmit={onSubmit} processing={form.isProcessing}>
             <div className="grid grid-cols-6 gap-6">
 
               <div className="col-span-6 sm:col-span-3 relative">
                 <label htmlFor="title" className="block text-sm font-bold text-gray-700">
                   Name <sup className='text-red-primary'>*</sup>
                 </label>
-                <input type="text" defaultValue={form.input.value('title')} onChange={form.input.onChange.title} name="title" id="title" className="mt-1 block w-full  border border-gray-300 py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm" />
-                <InputError error={form.errors?.title} />
+                <input type="text" defaultValue={form.input.title} onChange={form.onChange('title')} name="title"
+                       id="title"
+                       className="mt-1 block w-full  border border-gray-300 py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"/>
+                <InputError error={form.errors('title')}/>
               </div>
 
               <div className="col-span-6 sm:col-span-3">
                 <label htmlFor="attachment" className="block text-sm font-bold text-gray-700">
                   Image
                 </label>
-                <input type="file" accept={'image/*'} name={'attachment'} id={'attachment'} onChange={form.input.onChange.attachment} className={'mt-1 p-0.5 block w-full border border-gray-300 text-sm text-slate-500 file:mr-4 file:py-1.5 file:px-4  file:border-0 file:text-sm file:font-semibold file:bg-red-50 file:text-red-700 hover:file:bg-red-100 focus:outline-none'} />
-                <InputError error={form.errors?.attachment} />
+                <input type="file" accept={'image/*'} name={'attachment'} id={'attachment'}
+                       onChange={form.onChange('attachment')}
+                       className={'mt-1 p-0.5 block w-full border border-gray-300 text-sm text-slate-500 file:mr-4 file:py-1.5 file:px-4  file:border-0 file:text-sm file:font-semibold file:bg-red-50 file:text-red-700 hover:file:bg-red-100 focus:outline-none'}/>
+                <InputError error={form.errors('attachment')}/>
               </div>
 
               <div className="col-span-6 sm:col-span-2">
                 <label htmlFor="page" className="block text-sm font-bold text-gray-700">
                   Page
                 </label>
-                <select defaultValue={loader.response.banner.options.page} onChange={form.input.onChange.options.page} id="page" name="page" className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm disabled:bg-gray-50">
+                <select defaultValue={loader.response.banner.options.page} onChange={form.onChange('page')} id="page"
+                        name="page"
+                        className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm disabled:bg-gray-50">
                   <option value={'home'}>Home</option>
                 </select>
+                <InputError error={form.errors('page')}/>
               </div>
 
               <div className="col-span-6 sm:col-span-2">
                 <label htmlFor="section" className="block text-sm font-bold text-gray-700">
                   Section
                 </label>
-                <select defaultValue={loader.response.banner.options.section} onChange={form.input.onChange.options.section} id="section" name="section" className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
+                <select defaultValue={loader.response.banner.options.section} onChange={form.onChange('section')}
+                        id="section" name="section"
+                        className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
                   <option value={'todayspick'}>Today's Pick</option>
                   <option value={'hero'}>Hero Section</option>
                 </select>
+                <InputError error={form.errors('section')}/>
               </div>
 
               <div className="col-span-6 sm:col-span-2">
                 <label htmlFor="link" className="block text-sm font-bold text-gray-700">
                   Link
                 </label>
-                <input type="text" defaultValue={loader.response.banner.options.link} onChange={form.input.onChange.options.link} name="link" id="link" className="mt-1 block w-full  border border-gray-300 py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm" />
+                <input type="text" defaultValue={loader.response.banner.options.link} onChange={form.onChange('link')}
+                       name="link" id="link"
+                       className="mt-1 block w-full  border border-gray-300 py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"/>
+                <InputError error={form.errors('link')}/>
               </div>
 
               <div className="col-span-6 sm:col-span-3">
                 <label htmlFor="type" className="block text-sm font-bold text-gray-700">
                   Type
                 </label>
-                <select defaultValue={loader.response.banner.options.type} onChange={form.input.onChange.options.type} id="type" name="type" className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
+                <select defaultValue={loader.response.banner.options.type} onChange={form.onChange('type')} id="type"
+                        name="type"
+                        className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
                   <option value={'image'}>Image</option>
                   <option value={'slide'}>Slide</option>
                 </select>
+                <InputError error={form.errors('type')}/>
               </div>
 
               <div className="col-span-6 sm:col-span-3">
                 <label htmlFor="status" className="block text-sm font-bold text-gray-700">
                   Status <sup className='text-red-primary'>*</sup>
                 </label>
-                <select defaultValue={loader.response.banner.status} id="status" onChange={form.input.onChange.status} name="status" className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
+                <select defaultValue={loader.response.banner.status} id="status" onChange={form.onChange('status')}
+                        name="status"
+                        className="mt-1 block w-full border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
                   <option value={'active'}>Active</option>
                   <option value={'in-active'}>In-Active</option>
                 </select>
-                <InputError error={form.errors?.status} />
+                <InputError error={form.errors('status')}/>
               </div>
 
             </div>
