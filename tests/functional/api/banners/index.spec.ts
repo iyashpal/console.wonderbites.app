6ace6e446a32cc4f9bd6872040e87c0156a05777
iyashpal <iyashpal.dev@gmail.com>
2023-07-14 16:59:38 +0530

    chore(new) Test cases added to banners API.

--- tests/functional/api/banners/index.spec.ts
+++ tests/functional/api/banners/index.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@japa/runner'
 import Database from '@ioc:Adonis/Lucid/Database'
-import { BannerFactory } from 'Database/factories'
+import { BannerFactory, UserFactory } from 'Database/factories'
 
 test.group('Api [banners.index]', (group) => {
   group.each.setup(async () => {
@@ -12,5 +12,19 @@ test.group('Api [banners.index]', (group) => {
     const banner = await BannerFactory.with('user').create()
     const $response = await client.get(route('api.banners.index', {}, { qs: {with: ['banners.user']} }))
     $response.assertBodyContains([{id: banner.id}])
-  }).tags(['api.banners.index'])
+  }).tags(['@api', '@api.banners', '@api.banners.index'])
+
+  test('it allows access to guest users to see the banners list .', async ({ client, route }) => {
+    const banner = await BannerFactory.with('user').create()
+    const $response = await client.get(route('api.banners.index', {}, { qs: {with: ['banners.user']} }))
+    $response.assertBodyContains([{id: banner.id}])
+  }).tags(['@api', '@api.banners', '@api.banners.index'])
+
+  test('it allows access to logged-in users to see the banners list .', async ({ client, route }) => {
+    const user = await UserFactory.create()
+    const banner = await BannerFactory.with('user').create()
+    const $response = await client.get(route('api.banners.index', {}, { qs: { with: ['banners.user'] } }))
+      .guard('api').loginAs(user)
+    $response.assertBodyContains([{id: banner.id}])
+  }).tags(['@api', '@api.banners', '@api.banners.index'])
 })
