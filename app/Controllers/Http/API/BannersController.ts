83714b554f230af31eab81f7746e97ba4faaec36
iyashpal <iyashpal.dev@gmail.com>
2023-07-14 16:58:58 +0530

    chore(new) Error response updated based on new error exception implementation.

--- app/Controllers/Http/API/BannersController.ts
+++ app/Controllers/Http/API/BannersController.ts
@@ -1,39 +1,25 @@
-import {BannerBuilder} from 'App/Helpers/Database'
-import ErrorJSON from 'App/Helpers/ErrorJSON'
+import { builder } from 'App/Helpers/Database'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class BannersController {
   public async index ({request, response}: HttpContextContract) {
     try {
-      const banners = await (new BannerBuilder(request))
-
-        .resolve('banners').query()
+      const banners = await builder('Banner', request, 'banners').query()
 
       response.json(banners)
     } catch (error) {
-      response.status(error.status).json(new ErrorJSON(error))
+      throw error
     }
   }
-
-  public async create ({}: HttpContextContract) {}
-
-  public async store ({}: HttpContextContract) {}
 
   public async show ({request, params, response}: HttpContextContract) {
     try {
-      const banner = await (new BannerBuilder(request))
-
-        .resolve('banner').query().where('id', params.id).firstOrFail()
+      const banner = await builder('Banner', request, 'banner').query()
+        .where('id', params.id).firstOrFail()
 
       response.json(banner)
     } catch (error) {
-      response.status(error.status).json(new ErrorJSON(error))
+      throw error
     }
   }
-
-  public async edit ({}: HttpContextContract) {}
-
-  public async update ({}: HttpContextContract) {}
-
-  public async destroy ({}: HttpContextContract) {}
 }
