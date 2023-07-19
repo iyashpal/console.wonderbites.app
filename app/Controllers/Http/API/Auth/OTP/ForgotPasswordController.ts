39173927811e10525c0380d247787bf657aa00c0
Yash Pal <iyashpal.dev@gmail.com>
2023-07-19 19:16:16 +0530

    chore(new) OTP helper replaced with Event Dispatch.

--- app/Controllers/Http/API/Auth/OTP/ForgotPasswordController.ts
+++ app/Controllers/Http/API/Auth/OTP/ForgotPasswordController.ts
@@ -1,10 +1,10 @@
-import { DateTime } from 'luxon'
-import OTP from 'App/Helpers/OTP'
+import {DateTime} from 'luxon'
 import Env from '@ioc:Adonis/Core/Env'
-import { string } from '@ioc:Adonis/Core/Helpers'
-import { User, VerificationCode } from 'App/Models'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {string} from '@ioc:Adonis/Core/Helpers'
+import {User, VerificationCode} from 'App/Models'
+import type {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import ForgotPasswordValidator from 'App/Validators/API/Auth/OTP/ForgotPasswordValidator'
+import Event from '@ioc:Adonis/Core/Event'
 
 export default class ForgotPasswordController {
   public async handle ({request, response}: HttpContextContract) {
@@ -19,24 +19,24 @@ export default class ForgotPasswordController {
       ).firstOrFail()
 
       const code = await VerificationCode.updateOrCreate(
-        { source: email ?? mobile, userId: user.id, verifiedAt: null },
+        {source: email ?? mobile, userId: user.id, verifiedAt: null},
         {
           token: string.generateRandom(32),
-          expiresAt: DateTime.now().plus({ minute: 10 }),
+          expiresAt: DateTime.now().plus({minute: 10}),
           code: Math.floor(1000 + Math.random() * 9000).toString(),
         }
       )
 
       if (code && ['production'].includes(Env.get('NODE_ENV'))) {
         try {
-          await (new OTP()).viaMail(user.email, code.code)
-          await (new OTP()).viaSMS(user.mobile, code.code, identifier)
+          await Event.emit('OneTimePassword:EMAIL', {source: code.source, code: code.code})
+          await Event.emit('OneTimePassword:SMS', {source: code.source, identifier, code: code.code})
         } catch (error) {
           throw error
         }
       }
 
-      response.ok({ success: !!code.id, token: code.token, source: email ?? mobile, user: user.id })
+      response.ok({success: !!code.id, token: code.token, source: email ?? mobile, user: user.id})
     } catch (error) {
       throw error
     }
--- app/Controllers/Http/API/Auth/OTP/GenerateCodesController.ts
+++ app/Controllers/Http/API/Auth/OTP/GenerateCodesController.ts
@@ -1,33 +1,33 @@
-import { DateTime } from 'luxon'
-import OTP from 'App/Helpers/OTP'
+import {DateTime} from 'luxon'
 import Env from '@ioc:Adonis/Core/Env'
-import { string } from '@ioc:Adonis/Core/Helpers'
-import { User, VerificationCode } from 'App/Models'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Event from '@ioc:Adonis/Core/Event'
+import {string} from '@ioc:Adonis/Core/Helpers'
+import {User, VerificationCode} from 'App/Models'
+import type {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import GenerateValidator from 'App/Validators/API/Auth/OTP/GenerateValidator'
 
 export default class GenerateCodesController {
-  public async handle ({ request, response }: HttpContextContract) {
+  public async handle ({request, response}: HttpContextContract) {
     try {
-      const { source, identifier = '' } = await request.validate(GenerateValidator)
+      const {source, identifier = ''} = await request.validate(GenerateValidator)
 
       const user = await User.query().where('mobile', source).orWhere('email', source).first()
 
       const code = await VerificationCode.updateOrCreate(
-        { source, userId: user?.id ?? null, verifiedAt: null },
+        {source, userId: user?.id ?? null, verifiedAt: null},
         {
           token: string.generateRandom(32),
-          expiresAt: DateTime.now().plus({ minute: 10 }),
+          expiresAt: DateTime.now().plus({minute: 10}),
           code: Math.floor(1000 + Math.random() * 9000).toString(),
         }
       )
 
       if (code && ['production'].includes(Env.get('NODE_ENV'))) {
-        await (new OTP()).viaMail(source, code.code)
-        await (new OTP()).viaSMS(source, code.code, identifier)
+        await Event.emit('OneTimePassword:EMAIL', {source, code: code.code})
+        await Event.emit('OneTimePassword:SMS', {source, identifier, code: code.code})
       }
 
-      response.ok({ success: !!code.id, token: code.token, source, ...(user ? { user: user.id } : {}) })
+      response.ok({success: !!code.id, token: code.token, source, ...(user ? {user: user.id} : {})})
     } catch (error) {
       throw error
     }
