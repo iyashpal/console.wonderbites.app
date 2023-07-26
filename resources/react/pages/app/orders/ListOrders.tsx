0f3fa41bb6fcfafbdebbba53daefd6d16a6dcfca
Yash Pal <iyashpal.dev@gmail.com>
2023-07-26 19:22:10 +0530

    chore(new) Update order status field added to order list.

--- resources/react/pages/app/orders/ListOrders.tsx
+++ resources/react/pages/app/orders/ListOrders.tsx
@@ -1,20 +1,22 @@
 import { DateTime } from "luxon";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Icons from '~/helpers/icons'
 import { Order } from "~/contracts/schema";
-import { useDataLoader } from "@/hooks";
+import { useDataLoader, useForm } from "@/hooks";
 import { MetaData } from "@/contracts/pagination";
 import Resources from "@/components/resources";
 import Breadcrumb from "~/layouts/AuthLayout/Breadcrumb";
 import { Link, useLocation, useSearchParams } from "react-router-dom";
+import { Select } from "@/components/forms";
 
 const columns = [
-  { name: 'Order #', options: { className: 'text-center' } },
+  { name: 'Order #', options: { className: 'text-center whitespace-nowrap' } },
   { name: 'Date/Time', options: { className: 'text-left' } },
   { name: 'Customer', options: { className: 'text-left' } },
   { name: 'Type', options: { className: 'text-left' } },
   { name: 'Total', options: { className: 'text-center' } },
-  { name: 'Status', options: { className: 'text-left' } },
+  { name: 'Address', options: { className: 'text-left' } },
+  { name: 'Status', options: { className: 'text-left w-44' } },
 ]
 const sortByFilters = [
   { label: 'ID', value: 'id' },
@@ -54,7 +56,8 @@ export default function ListOrders() {
             </>),
             <span className="uppercase">{order.order_type}</span>,
             `-`,
-            <span className="uppercase">{order.status}</span>,
+            [order.street, '', order.city].filter(item => item).join(', '),
+            <Status order={order} />,
             <div className="flex item-center justify-center gap-x-1">
               <Link to={`/app/orders/${order.id}`} className={'bg-gray-100 border border-gray-400 text-gray-500 rounded-lg p-1 hover:border-green-700 hover:bg-green-100 hover:text-green-700 transition-colors ease-in-out duration-300'}>
                 <Icons.Outline.Eye className={'w-5 h-5'} />
@@ -65,4 +68,38 @@ export default function ListOrders() {
       </div>
     </div>}
   </>
+}
+
+function Status({ order }: { order: Order }) {
+  const [state, setState] = useState(order.status)
+  const form = useForm({ status: order.status })
+
+  const items = {
+    placed: 'Placed',
+    cancelled: order.status === 'cancelled' ? 'Cancelled' : 'Cancel',
+    confirmed: 'Confirmed',
+    preparing: 'Preparing',
+    'in-transit': 'Out For Delivery',
+    delivered: 'Delivered',
+  }
+
+  function is(status) {
+    return state === status;
+  }
+
+  function onChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    form.put(`/orders/${order.id}`, { data: { status: e.target.value } }).then(() => {
+      setState(e.target.value)
+    })
+  }
+
+  return (
+    <Select disabled={['delivered', 'cancelled'].includes(state)} defaultValue={state} onChange={onChange}>
+      {Object.keys(items).map((key, index) => (
+        <option key={index} value={key} disabled={is(key)} className={`${is(key) ? 'text-red-primary' : ''}`}>
+          {items[key]}
+        </option>
+      ))}
+    </Select>
+  )
 }
